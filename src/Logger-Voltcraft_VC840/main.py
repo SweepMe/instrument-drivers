2553b0827c34758a08b9262d4441a84794be56a4
Axel Fischer <axel.fischer@sweep-me.net>
2023-12-13 18:15:22 +0100

    Voltcraft VC840 -> license updated, some code cleanup

--- src/Logger-Voltcraft_VC840/main.py
+++ src/Logger-Voltcraft_VC840/main.py
@@ -5,7 +5,7 @@
 #
 # MIT License
 # 
-# Copyright (c) 2019 - 2020 Axel Fischer (sweep-me.net)
+# Copyright (c) 2023 SweepMe! GmbH (sweep-me.net)
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -32,16 +32,18 @@
 
 """
 A Device Class to readout a Voltcraft Multimeter model VC840 or similar device.
-The communication is based on COM port communication using a FS9721-LPX protocol. Any other multimeter using this standard can be read out as well.
+The communication is based on COM port communication using a FS9721-LPX protocol.
+Any other multimeter using this standard can be read out as well.
 Switching the mode of the multimeter during a measurement is not recommended any may lead to errorneous values.
 The first return value is a float of the displayed value. The second value is a string of the momentary unit.
 """
 
-import os, sys
+import os
+import sys
 import time
-from ErrorMessage import error
+from pysweepme.ErrorMessage import error
+from pysweepme.EmptyDeviceClass import EmptyDevice
 
-from EmptyDeviceClass import EmptyDevice # Class comes with SweepMe!
 
 class Device(EmptyDevice):
 
@@ -65,34 +67,34 @@ class Device(EmptyDevice):
                                 }
                                 
         self.all = {
-                    1  : ["RS232", "AUTO", "DC", "AC"],
-                    2  : ["A1", "A6", "A5", "Minus"],
-                    3  : ["A2", "A7", "A3", "A4"],
-                    4  : ["B1", "B6", "B5", "P1"],
-                    5  : ["B2", "B7", "B3", "B4"],
-                    6  : ["C1", "C6", "C5", "P2"],
-                    7  : ["C2", "C7", "C3", "C4"],
-                    8  : ["D1", "D6", "D5", "P3"],
-                    9  : ["D2", "D7", "D3", "D4"],
-                    10 : ["Diode", "k", "n", "µ"],
-                    11 : ["Beep", "M", "%", "m"],
-                    12 : ["Hold", "Delta", "Ohm", "F"],
-                    13 : ["Batt.", "Hz", "V", "A"],
-                    14 : ["°C", "", "", ""],
+                    1 : ["RS232", "AUTO", "DC", "AC"],
+                    2 : ["A1", "A6", "A5", "Minus"],
+                    3 : ["A2", "A7", "A3", "A4"],
+                    4 : ["B1", "B6", "B5", "P1"],
+                    5 : ["B2", "B7", "B3", "B4"],
+                    6 : ["C1", "C6", "C5", "P2"],
+                    7 : ["C2", "C7", "C3", "C4"],
+                    8 : ["D1", "D6", "D5", "P3"],
+                    9 : ["D2", "D7", "D3", "D4"],
+                    10: ["Diode", "k", "n", "µ"],
+                    11: ["Beep", "M", "%", "m"],
+                    12: ["Hold", "Delta", "Ohm", "F"],
+                    13: ["Batt.", "Hz", "V", "A"],
+                    14: ["°C", "", "", ""],
                     }
                     
         self.numbers = {
-                    0 : [1, 2, 3, 4, 5, 6], 
-                    1 : [2, 3],
-                    2 : [1, 2, 4, 5, 7],
-                    3 : [1, 2, 3, 4, 7],
-                    4 : [2, 3, 6, 7],
-                    5 : [1, 3, 4, 6, 7],
-                    6 : [1, 3, 4, 5, 6, 7],
-                    7 : [1, 2 ,3],
-                    8 : [1, 2, 3, 4, 5, 6, 7],
-                    9 : [1, 2, 3, 4, 6, 7],
-                    "L" : [4,5,6],
+                    0: [1, 2, 3, 4, 5, 6],
+                    1: [2, 3],
+                    2: [1, 2, 4, 5, 7],
+                    3: [1, 2, 3, 4, 7],
+                    4: [2, 3, 6, 7],
+                    5: [1, 3, 4, 6, 7],
+                    6: [1, 3, 4, 5, 6, 7],
+                    7: [1, 2 ,3],
+                    8: [1, 2, 3, 4, 5, 6, 7],
+                    9: [1, 2, 3, 4, 6, 7],
+                    "L": [4, 5, 6],
                     }
                     
         self.unit_types = ["Ohm", "F", "Hz", "V", "A", "°C", "%"]
@@ -104,20 +106,17 @@ class Device(EmptyDevice):
                             "k" : 1e3,
                             "M" : 1e6,
                            }
-                           
-                           
-        self.readout_timeout = 2.0 # in seconds
-        
-                
-        
+
+        self.readout_timeout = 2.0  # in seconds
+
     def get_GUIparameter(self, parameter):
         self.port = parameter["Port"]
                                 
     def call(self):
-    
-    
+
         # this routine makes sure that old messages are thrown away
-        # readout is always done until the last byte (14) to make sure a value reading is related to a correlated set of bytes
+        # readout is always done until the last byte (14) to make sure a value reading is
+        # related to a correlated set of bytes
         while self.port.in_waiting() > 14:
         
             for i in range(14):
@@ -158,7 +157,6 @@ class Device(EmptyDevice):
                 # if the last byte is reached and the first byte is also in, we can assume that a full data set is acquired
                 if byte_index == 14 and 1 in index_dict.keys():
                     break
-                    
 
             if time.perf_counter() - starttime > self.readout_timeout:
                 return [float('nan'), 'no value']
@@ -166,8 +164,7 @@ class Device(EmptyDevice):
                 
         infos_values = list(infos.keys())
         #print(infos_values)
-                    
-        
+
         # There are four 7-digit-display segments, being A, B, C, and D
         # here, for each numbers the dashes used to construct the 7-digit number are appended to individual lists
         
@@ -178,7 +175,8 @@ class Device(EmptyDevice):
         
         #print(As,Bs,Cs,Ds)
         
-        # The combination of these dashes of the each 7-digit-segment can be used to reconstruct the real vale using self.numbers
+        # The combination of these dashes of the each 7-digit-segment can be used to
+        # reconstruct the real value using self.numbers
         for key in self.numbers:
             if sorted(As) == self.numbers[key]:
                 A = key  
@@ -198,22 +196,19 @@ class Device(EmptyDevice):
             if sorted(Ds) == self.numbers[key]:
                 D = key  
                 break
-          
-          
+
         try:
-            if B == 0 and C == "L": # Overload
+            if B == 0 and C == "L":  # Overload
                 
-                #print("B:", type(B), B)
+                # print("B:", type(B), B)
                 self.result = float('nan')
                 unit = "overload"
                 
                 return [self.result, unit]
-                
         except:
             self.result = float('nan')
             unit = "error"
-           
-           
+
         # get result  
         try:
             self.result = A*1000.0 + B*100.0 + C*10.0 + D*1.0
@@ -238,8 +233,7 @@ class Device(EmptyDevice):
             self.result = float('nan')
             unit = "error"
             return [self.result, unit]
-         
-         
+
         # find unit
         unit = ""
         for key in self.unit_types:
@@ -248,5 +242,3 @@ class Device(EmptyDevice):
                 break
         
         return [self.result, unit]
-
-    
