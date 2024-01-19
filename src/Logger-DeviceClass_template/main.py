0d4298796f282eda20500f67196071a6f0798477
Felix Kaschura <felix.kaschura@sweep-me.net>
2024-01-19 10:51:47 +0100

    format comments

--- src/Logger-DeviceClass_template/main.py
+++ src/Logger-DeviceClass_template/main.py
@@ -5,7 +5,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2022 SweepMe! GmbH
+# Copyright (c) 2024 SweepMe! GmbH (sweep-me.net)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -27,39 +27,39 @@
 
 
 # SweepMe! device class
-# Type: Logger
-# Device: Template
+# * Type: Logger
+# * Device: Template
 
 
-## !!! Change the above license to your case and needs. !!!
-## Although the above MIT license states that you have to add it to copies, we do not insist in that. Please feel free to not add the MIT license of this file.
+### !!! Change the above license to your case and needs. !!!
+### Although the above MIT license states that you have to add it to copies,
+### we do not insist in that. Please feel free to not add the MIT license of this file.
 
-## Each device class must have a name like: "<Module>-<Manufacturer>_<Model>".
-
-
-## New drivers can also be created as a service. Please write to "contact@sweep-me.net" if you need a new driver of if you need support with creating one.
+### Each device class must have a name like: "<Module>-<Manufacturer>_<Model>".
 
 
-## use the next two lines to add the folder of this device class to the PATH variable
-# from FolderManager import addFolderToPATH
-# addFolderToPATH()
+### New drivers can also be created as a service.
+### Please write to "contact@sweep-me.net" if you need a new driver of if you need support with creating one.
 
 
-## if you use 'addFolderToPATH', you can now important packages that are shipped with your device class
-
-
-# import further python module here as usual, many packages come with SweepMe!, all other have to be shipped with the device class
+### import further python module here as usual, many packages come with SweepMe!,
+### all other have to be shipped with the device class
 import pathlib
 import random
 
+### If you like to see the source code of EmptyDevice, take a look at the pysweepme package that contains this file
 from pysweepme.EmptyDeviceClass import EmptyDevice
 from pysweepme.ErrorMessage import debug, error
 
-# If you like to see the source code of EmptyDevice, take a look at the pysweepme package that contains this file
+### use the next two lines to add the folder of this device class to the PATH variable
+# from FolderManager import addFolderToPATH
+# addFolderToPATH()
+### if you use 'addFolderToPATH', you can now important packages that are shipped with your device class
 
 
 class Device(EmptyDevice):
-    ## here you can add html formatted description to your device class that is shown by modules like 'Logger' or 'Switch' that have a description field.
+    ### here you can add html formatted description to your device class
+    ### that is shown by modules like 'Logger' or 'Switch' that have a description field.
     description = """
                     <h3>Template for a Logger Device Class</h3>
                     <p>---Modify this description text later to guide your users---</p>
@@ -92,7 +92,8 @@ class Device(EmptyDevice):
         ### Also works if self.port_manager is False or commented.
         # self.port_types = ["COM", "GPIB", "USB", "TCPIP"]
 
-        ### use/uncomment the next lines to change port properties, you can find all keys here: https://wiki.sweep-me.net/wiki/Port_manager#Port_properties
+        ### use/uncomment the next lines to change port properties,
+        ### you can find all keys here: https://wiki.sweep-me.net/wiki/Port_manager#Port_properties
         # self.port_properties = {
         #                           "timeout": 1,
         #                           "baudrate": 9600,
@@ -101,7 +102,8 @@ class Device(EmptyDevice):
 
     def set_GUIparameter(self):
         # add keys and values to generate GUI elements in the Parameters-Box
-        # If you use this template to create a driver for modules other than Logger or Switch, you need to use fixed keys that are defined for each module.
+        # If you use this template to create a driver for modules other than Logger or Switch,
+        # you need to use fixed keys that are defined for each module.
 
         GUIparameter = {
             "String": "test",
@@ -121,8 +123,9 @@ class Device(EmptyDevice):
             "Data path": pathlib.Path(),
         }
 
-        ### Tip:
-        ### Make sure that you do not use special strings such as 'Port', 'Comment', 'Device', 'Data', 'Description', or 'Parameters'
+        ### Caution:
+        ### Make sure that you do not use special strings
+        ### such as 'Port', 'Comment', 'Device', 'Data', 'Description', or 'Parameters'
         ### These strings have special meaning and should not be overwritten.
 
         return GUIparameter
@@ -139,27 +142,29 @@ class Device(EmptyDevice):
         # print(parameter["Float"])
         # print(parameter["Data path"])
 
-        ### the port selected by the user is readout here and saved in a variable that can be later used to open the correct port
+        ### the port selected by the user is readout here and saved in a variable
+        ### that can be later used to open the correct port
         self.port_string = parameter["Port"]  # use this string to open the right port object later during 'connect'
         # print("Selected port", self.port_string)
 
     def find_ports(self):
-        """\
-        this function is called whenever the user presses 'Find ports' button.
+        """This function is called whenever the user presses 'Find ports' button.
+
         No need to use, if you search for ports using self.port_types in __init__
         Function can be removed if not needed.
         """
         print("find_ports")
 
-        ### if you do not use the port manager you can use this function to return a list of strings with possible port items
+        ### if you do not use the port manager you can use this function
+        ### to return a list of strings with possible port items
 
         ### the next lines are an example how to find ports yourself
         import pyvisa
 
         try:
-            rm = (
-                pyvisa.ResourceManager()
-            )  # if your dll is not found and you would like to try a different path use: rm = visa.ResourceManager("C:\\Windows\\System32\\visa32.dll")
+            # if your dll is not found and you would like to try a different path use:
+            # rm = visa.ResourceManager("C:\\Windows\\System32\\visa32.dll")
+            rm = (pyvisa.ResourceManager())
 
             print("ResourceManager:", rm)
 
@@ -178,10 +183,10 @@ class Device(EmptyDevice):
 
             return ["Port1", "Port2"]
 
-    #### ----------------------------------------------------------------------------------------------------------------------
+    ### --------------------------------------------------------------------------------------------
     """ here, semantic standard functions start that are called by SweepMe! during a measurement """
 
-    ### all functions are overload functions that are called by SweepMe!
+    ### all functions are overridden functions that are called by SweepMe!
     ### remove those function that you do not needed
 
     def connect(self):
@@ -191,13 +196,14 @@ class Device(EmptyDevice):
         print(60 * "-")
         print("connect")
 
-        #### if you do not use the port manager you can also create your own port objects
+        ### if you do not use the port manager you can also create your own port objects
         # rm = visa.ResourceManager()
         # self.instrument = rm.open_resource(self.port_string)
         # print("Instrument identification:", self.instrument.query('*IDN?'))
 
         ### of course you can use any other library to open your ports, e.g. pyserial
-        ### just use 'find_Ports' to find all possible ports and then open them here based on the string 'self.port_string' that is created during 'get_GUIparameter'
+        ### just use 'find_Ports' to find all possible ports and then open them here
+        ### based on the string 'self.port_string' that is created during 'get_GUIparameter'
 
     def disconnect(self):
         # called only once at the end of the measurement
@@ -214,26 +220,30 @@ class Device(EmptyDevice):
 
         # In 'initialize' you can check whether the user input is valid. If not you can abort the run by using the lines below
         # self.stop_measurement("Value of ... not valid. Please use ...") # this function adds a stop message that is displays in an info box
-        # return False # Any semantic standard function that returns with Falses stops the measurement
+        # return False  # Any semantic standard function that returns with False stops the measurement
 
     def deinitialize(self):
         # called only once at the end of the measurement
         print("deinitialize")
 
     def poweron(self):
-        # called if the measurement procedure enters a branch of the sequencer and the module has not been used in the previous branch
+        # called if the measurement procedure enters a branch of the sequencer
+        # and the module has not been used in the previous branch
         print("poweron")
 
     def poweroff(self):
-        # called if the measurement procedure leaves a branch of the sequencer and the module is not used in the next branch
+        # called if the measurement procedure leaves a branch of the sequencer
+        # and the module is not used in the next branch
         print("poweroff")
 
     def configure(self):
-        # called if the measurement procedure enters a branch of the sequencer and the module has not been used in the previous branch
+        # called if the measurement procedure enters a branch of the sequencer
+        # and the module has not been used in the previous branch
         print("configure")
 
     def unconfigure(self):
-        # called if the measurement procedure leaves a branch of the sequencer and the module is not used in the next branch
+        # called if the measurement procedure leaves a branch of the sequencer
+        # and the module is not used in the next branch
         print("unconfigure")
 
     def signin(self):
@@ -258,9 +268,10 @@ class Device(EmptyDevice):
     ###------------------------------------------------------------------
     """ the following functions are called for each measurement point """
 
-    #### these functions can be used to organize the sequence of all instruments that are part of one branch of the sequencer
-    #### each function is called for all devices and then the next function is processed
-    #### remove functions that are not needed
+    ### these functions can be used to organize the sequence of all instruments
+    ### that are part of one branch of the sequencer
+    ### each function is called for all devices and then the next function is processed
+    ### remove functions that are not needed
 
     def start(self):
         """'start' can be used to do some first steps before the acquisition of a measurement point starts."""
@@ -268,13 +279,15 @@ class Device(EmptyDevice):
 
     def apply(self):
         """'apply' is used to set the new setvalue that is always available as 'self.value'."""
-        # apply is not called in the module 'Logger' as as logger cannot apply any value, but it can be used in all other modules that have varying sweep values
+        # apply is not called in the module 'Logger' as as logger cannot apply any value,
+        # but it can be used in all other modules that have varying sweep values
         # apply is only called if the setvalue ("Sweep value") has changed
         print("apply")
 
         print("New value to apply:", self.value)
         # self.value is a variable created by SweepMe! and stores the latest sweep value that should be applied
-        # It can be any object. Please make sure to to test the type and change it to the format you need before you send it to a device.
+        # It can be any object. Please make sure to to test the type
+        # and change it to the format you need before you send it to a device.
 
     def reach(self):
         """'reach' can be added to make sure the latest setvalue applied during 'apply' is reached."""
@@ -286,14 +299,20 @@ class Device(EmptyDevice):
         print("adapt")
 
     def adapt_ready(self):
-        """'adapt_ready' can be used to make sure that a procedure started in 'adapt' is finished. Thus, multiple instrument can start an adapt-procedure simultaneously."""
-        print("adapt")
+        """'adapt_ready' can be used to make sure that a procedure started in 'adapt' is finished.
+
+        Thus, multiple instrument can start an adapt-procedure simultaneously.
+        """
+        print("adapt_ready")
 
     def trigger_ready(self):
         print("trigger_ready")
 
     def measure(self):
-        """'measure' should be used to trigger the acquisition of new data. If all drivers use this function for this purpose, the data acquisition can start almost simultaneously."""
+        """'measure' should be used to trigger the acquisition of new data.
+
+        If all drivers use this function for this purpose, the data acquisition can start almost simultaneously.
+        """
         print("measure")
 
     def request_result(self):
@@ -309,11 +328,12 @@ class Device(EmptyDevice):
         print("process_data")
 
     def call(self):
-        """\
-        'call' is a mandatory function that must be used to return as many values as defined in self.variables
+        """'call' is a mandatory function that must be used to return as many values as defined in self.variables.
+
         This function can only be omitted if no variables are defined in self.variables.
         """
-        # most import function: return exactly the number of values that have been defined by self.variables and self.units
+        # most import function:
+        # return exactly the number of values that have been defined by self.variables and self.units
         print("call")
 
         value1 = random.random()
