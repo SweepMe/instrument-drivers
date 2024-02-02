0f5ca0cc694e21e8b4a3ff623df404e5411c0c33
Axel Fischer <axel.fischer@sweep-me.net>
2024-02-02 14:27:31 +0100

    Keithley 2450: License updated

--- src/SMU-Keithley_2450/main.py
+++ src/SMU-Keithley_2450/main.py
@@ -5,7 +5,7 @@
 #
 # MIT License
 # 
-# Copyright (c) 2018 - 2019 Axel Fischer (sweep-me.net)
+# Copyright (c) 2024 SweepMe! GmbH (sweep-me.net)
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
