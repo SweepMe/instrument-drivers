53e7fd0840e8fcbeea9aa87dbdf84f0b6eaef008
Franz Hempel <franz.hempel@sweep-me.net>
2024-02-15 17:21:49 +0100

    Camera-Basler -> Create Skeleton

--- src/Camera-Basler_pylon/main.py
+++ src/Camera-Basler_pylon/main.py
@@ -33,35 +33,59 @@ from pysweepme.EmptyDeviceClass import EmptyDevice
 
 addFolderToPATH()
 
-import pypylon
-
+from pypylon import pylon
 
 
 class Device(EmptyDevice):
     description = """
-    Description
-                """
+    This device class is a wrapper for the Basler pylon camera. It is based on the pypylon library.
+    """
 
-    def __init__(self):
+    def __init__(self) -> None:
         EmptyDevice.__init__(self)
 
         self.shortname = "Camera Basler"  # short name will be shown in the sequencer
+        self.instance_key: str
         self.variables = []
         self.units = []
 
+        # Acquistion Parameters
+        self.trigger_source: str
+        self.gain_auto: str
+        self.gain = None
+        self.exposure_auto: str
+        self.exposure_time: float
+        self.file_format: str
 
-    def set_GUIparameter(self):
+        # Image Parameters
+        self.preset: str
+        self.gamma = None
+        self.balance_ratio_red = None
+        self.balance_ratio_green = None
+        self.balance_ratio_blue = None
+
+        # Camera parameters
+        self.tlf = pylon.TlFactory.GetInstance()
+        self.cameras_dict: dict = {}
+        self.camera = None
+
+    def set_GUIparameter(self) -> dict:
         gui_parameter = {
             "SweepMode": ["None", "Exposure time [s]"],
             "Trigger": ["Software"],
             "GainAuto": ["Off", "Once", "Continuous"],
             "ExposureAuto": ["Off", "Once", "Continuous"],
-            "PixelFormat": list(self.PixelBytes.keys()),
+            # "PixelFormat": list(self.PixelBytes.keys()),
             "FileFormat": ["tiff", "tiff + jpg", "tiff + png", "jpg", "png"],
             "Gamma": 1,
             "ExposureTime": 0.1,
             "Gain": 1,
-            "Preset": ["Off", "Daylight5000K (Gamma * 0.45)", "Daylight6500K (Gamma * 0.45)", "Tungsten2800K (Gamma * 0.45)"],
+            "Preset": [
+                "Off",
+                "Daylight5000K (Gamma * 0.45)",
+                "Daylight6500K (Gamma * 0.45)",
+                "Tungsten2800K (Gamma * 0.45)",
+            ],
             "BalanceRatioRed": 1.0,
             "BalanceRatioGreen": 1.0,
             "BalanceRatioBlue": 1.0,
@@ -69,58 +93,100 @@ class Device(EmptyDevice):
 
         return gui_parameter
 
-    def get_GUIparameter(self, parameter):
+    def get_GUIparameter(self, parameter) -> None:
         """Parse and store GUI options."""
-        if parameter["Port"] != "No Camera connected!" and parameter["Port"] != "":
-            self.port = int(parameter["Port"].split(" ")[0])
-        else:
-            self.port = -1
-
-        self.SelectedFileFormats = parameter["FileFormat"]
-
-        for key in self.FileFormats.keys():
-            if key in self.SelectedFileFormats:
-                self.variables.append("%s image path" % key)
-                self.units.append("")
-                self.plottype.append(False)
-                self.savetype.append(False)
+        self.camera_name = parameter["Port"]
 
         self.sweepmode = parameter["SweepMode"]
-
         if self.sweepmode == "Exposure time [s]":
             self.variables.append("Exposure time")
             self.units.append("s")
             self.plottype.append(True)
             self.savetype.append(True)
 
-        self.parameter = parameter
+        # Acquistion Parameters
+        self.trigger_source = str(parameter["Trigger"])
+        self.gain_auto = str(parameter["GainAuto"])
+        self.gain = parameter["Gain"]
+        self.exposure_auto = str(parameter["ExposureAuto"])
+        self.exposure_time = float(parameter["ExposureTime"])
+        self.file_format = parameter["FileFormat"]
 
-    def find_ports(self):
-        pass
+        # Image Parameters
+        self.preset = str(parameter["Preset"])
+        self.gamma = parameter["Gamma"]
+        self.balance_ratio_red = parameter["BalanceRatioRed"]
+        self.balance_ratio_green = parameter["BalanceRatioGreen"]
+        self.balance_ratio_blue = parameter["BalanceRatioBlue"]
 
-    def connect(self):
-        pass
+        # self.SelectedFileFormats = parameter["FileFormat"]
+        # for key in self.FileFormats.keys():
+        #     if key in self.SelectedFileFormats:
+        #         self.variables.append("%s image path" % key)
+        #         self.units.append("")
+        #         self.plottype.append(False)
+        #         self.savetype.append(False)
 
-    def disconnect(self):
-        pass
+    def find_ports(self) -> list[str]:
+        self.get_cameras_dict()
+        return list(self.cameras_dict.keys())
 
-    def initialize(self):
-        pass
+    def connect(self) -> None:
+        self.get_cameras_dict()
 
-    def configure(self):
-        pass
-
-    def unconfigure(self):
         try:
-            self.imgBuf.Dispose()
-            self.imgBuf = None
-            self.buf.Dispose()
-            self.buf = None
-        except:
-            pass
+            _camera = self.cameras_dict[self.camera_name]
+            self.camera = pylon.InstantCamera(self.tlf.CreateDevice(_camera))
+            self.camera.Open()
 
-    def measure(self):
+            # Register camera in device_communication
+            self.instance_key = f"{self.shortname}_{self.camera_name}"
+            if self.instance_key not in self.device_communication:
+                self.device_communication[self.instance_key] = "Connected"
+
+        except Exception as e:
+            msg = f"Error opening connection to camera: {e!s}"
+            raise Exception(msg) from e
+
+    def disconnect(self) -> None:
+        if self.camera is not None:
+            self.camera.Close()
+
+            # Unregister camera in device_communication
+            self.device_communication.pop(self.instance_key)
+
+    def initialize(self) -> None:
         pass
 
-    def call(self):
-        return self.data
+    def configure(self) -> None:
+        pass
+
+    def unconfigure(self) -> None:
+        pass
+
+    def measure(self) -> None:
+        try:
+            # Starting the grabbing of c_countOfImagesToGrab images.
+            self.camera.StartGrabbingMax(1)
+            while self.camera.IsGrabbing():
+                grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+                if grabResult.GrabSucceeded():
+                    # Save the image data.
+                    self.image = grabResult.GetArray()
+                    print(self.image)
+                    # pylon.ImageFileHandler().Save(pylon.ImageFileFormat_Png, filename, img)
+                grabResult.Release()
+        except Exception as e:
+            msg = f"Error capturing image: {e!s}"
+            raise Exception(msg) from e
+
+    def call(self) -> None:
+        pass
+
+    "--- Convenience Functions ---"
+
+    def get_cameras_dict(self):
+        cameras = self.tlf.EnumerateDevices()
+        for cam in cameras:
+            name = cam.GetModelName()
+            self.cameras_dict[name] = cam
