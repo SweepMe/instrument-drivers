7d0e9ac80886db3215e863fd703e0e0af130e4a3
Axel Fischer <axel.fischer@sweep-me.net>
2024-10-22 17:19:40 +0200

    Creatio of aSpectSystems idSMU driver and first communication test

---
+++ src/SMU-aSpectSystems_idSMUx/libraries/libs_39_64/aspectdeviceengine/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+# from .enginecore import __doc__, __version__, IdSmuService
+from .enginecore import __doc__, __version__
+
+__all__ = ["__doc__", "__version__", "IdSmuService"]
