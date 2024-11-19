dd8e38c833eded4cd261815c57642c9f268768c3
Franz Hempel <franz.hempel@sweep-me.net>
2024-11-19 13:51:37 +0100

    Keithley 590 -> add command wrapper

--- src/LCRmeter-Keithley_590/main.py
+++ src/LCRmeter-Keithley_590/main.py
@@ -31,9 +31,6 @@
 
 from __future__ import annotations
 
-import os
-
-from pysweepme import FolderManager
 from pysweepme.EmptyDeviceClass import EmptyDevice
 
 
@@ -50,8 +47,9 @@ class Device(EmptyDevice):
         self.units = ["Ohm", "Ohm", "Hz", "V"]
         self.plottype = [True, True, True, True]
         self.savetype = [True, True, True, True]
-        self.identifier: str = ""
-        self.port_string: str = ""
+
+        self.port_manager = True
+        self.port_types = ["GPIB"]
 
         self.ranges = {
             "Auto": "R0",
@@ -67,22 +65,19 @@ class Device(EmptyDevice):
             "1MHz": "1MHz",
         }
 
-    def find_ports(self) -> list[str]:
-        """Return standard address."""
-        return ["15"]
-
     def set_GUIparameter(self) -> dict:  # noqa: N802
         """Set initial GUI parameter in SweepMe!."""
         return {
-            "SweepMode": ["None", "Frequency in Hz", "Voltage bias in V"],
-            "StepMode": ["None", "Frequency in Hz", "Voltage bias in V"],
-            "ValueRMS": 0.02,
-            "ValueBias": 0.0,
-            "Frequency": 1000.0,
-            "Integration": list(self.speeds),
-            "Trigger": ["Internal"],
-            "TriggerDelay": "0.1",
-            "Range": list(self.current_ranges),
+            "Address": "15",
+            # "SweepMode": ["None", "Frequency in Hz", "Voltage bias in V"],
+            # "StepMode": ["None", "Frequency in Hz", "Voltage bias in V"],
+            # "ValueRMS": 0.02,
+            # "ValueBias": 0.0,
+            # "Frequency": 1000.0,
+            # "Integration": list(self.speeds),
+            # "Trigger": ["Internal"],
+            # "TriggerDelay": "0.1",
+            # "Range": list(self.current_ranges),
         }
 
     def get_GUIparameter(self, parameter: dict) -> None:  # noqa: N802
@@ -135,10 +130,113 @@ class Device(EmptyDevice):
         return [self.resistance, self.reactance, self.measured_frequency, self.measured_dc_bias]
 
     """ here, convenience functions start """
+    # TODO: See Manual 4-19 for more commands
+
+    def set_frequency(self, frequency: float) -> None:
+        """Set the measurement frequency."""
+        frequencies = {
+            "100kHz": "F0",
+            "1MHz": "F1",
+        }
+        self.port.write("F0")
 
     def set_range(self, range: str) -> None:
         """Set the measurement range."""
+        ranges_100k = {
+            "Auto": "R0",
+            "2pF/2uS": "R1",
+            "20pF/20uS": "R2",
+            "200pF/200uS": "R3",
+            "2nF": "R4",
+            "R1 x 10": "R5",
+            "R2 x 10": "R6",
+            "R3 x 10": "R7",
+            "R4 x 10": "R8",
+            "Auto off": "R9",
+        }
 
+        ranges_1M = {
+            "Auto": "R0",
+            "20pF/200uS": "R1",
+            # "20pF/200uS": "R2",  # doubled?
+            "200pF/2mS": "R3",
+            "2nF/20mS": "R4",
+            "Auto off": "R9",
+        }
 
-        self.port.write("R0")
-        return "R0"
+        self.port.write("R0")  # auto
+
+    def set_reading_rate(self, rate: str) -> None:
+        """Set the reading rate."""
+        rates = {
+            "1000 /s": "S0",
+            "75 /s": "S1",
+            "18 /s": "S2",
+            "10 /s": "S3",
+            "1 /s": "S4",
+        }
+        self.port.write("S0")
+
+    def set_trigger_mode_and_source(self, mode: str, source: str) -> None:
+        """Set the trigger mode and source.
+
+        modes = {
+            "One-shot talk": "T0,0",
+            "Sweep, talk": "T0,1",
+            "One-shot, GET": "T1,0",
+            "Sweep, GET": "T1,1",
+            "One-shot, X": "T2,0",
+            "Sweep, X": "T2,1",
+            "One-shot, external": "T3,0",
+            "Sweep, external": "T3,1",
+            "One-shot, front panel": "T4,0",
+            "Sweep, front panel": "T4,1",
+        }
+        """
+        modes = {
+            "One-shot": "0",
+            "Sweep": "1",
+        }
+
+        sources = {
+            "Talk": "0",
+            "GET": "1",
+            "X": "2",
+            "External": "3",
+            "Front panel": "4",
+        }
+        mode = modes["One-shot"]
+        source = sources["GET"]
+
+        self.port.write(f"T{source},{mode}")
+
+    def set_zero_on(self, set_on: bool=True) -> None:
+        """Set the zero."""
+        command = "Z1" if set_on else "Z0"
+        self.port.write(command)
+
+    def set_filter_on(self, filter_on: bool=True) -> None:
+        """Set the filter."""
+        command = "P1" if filter_on else "P0"
+        self.port.write(command)
+
+    def set_voltage(self, voltage: float) -> None:
+        """Set the voltage."""
+        first = -voltage
+        last = voltage
+        step = voltage / 5
+
+        self.port.write(f"V {first},{last},{step}")
+
+    def set_waveform(self) -> None:
+        """0 DC, 1 Single Staircase, 2 Dual Staircase, 3 Pulse Train, 4 External bias source."""
+        waveform = 1
+        start = 1  # s
+        stop = 10
+        step = 0.1
+        self.port.write(f"W {waveform},{start},{stop},{step}")
+
+    def set_bias_on(self, bias_on: bool = True) -> None:
+        """Turn the voltage on."""
+        command = "N1" if bias_on else "N0"
+        self.port.write(command)
