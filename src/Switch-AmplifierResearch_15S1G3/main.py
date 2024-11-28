608e04cc6f2f534dfd8edf5d64ece8bec5a8fc58
Franz Hempel <franz.hempel@sweep-me.net>
2024-11-28 17:04:22 +0100

    AmplifierResearch -> initial commit

---
+++ src/Switch-AmplifierResearch_15S1G3/main.py
@@ -0,0 +1,114 @@
+# This Device Class is published under the terms of the MIT License.
+# Required Third Party Libraries, which are included in the Device Class
+# package for convenience purposes, may have a different license. You can
+# find those in the corresponding folders or contact the maintainer.
+#
+# MIT License
+#
+# Copyright (c) 2024 SweepMe! GmbH (sweep-me.net)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# SweepMe! driver
+# * Module: Switch
+# * Instrument: Amplifier Research 15S1G3
+
+from pysweepme.EmptyDeviceClass import EmptyDevice
+
+
+class Device(EmptyDevice):
+    """Child class to implement functionalities of a measurement device."""
+    description = """
+                    <h3>Amplifier Research 15S1G3</h3>
+                    <p>Setup:</p>
+                    <ul>
+                    <li>Enable remote control at device</li>
+                    <li>Set the device to the correct mode: Switch 6 in on position (1) enables RS-232, in off positio 
+                    (0) enables IEEE-488.</li>
+                    <li>Set the device to the correct baud rate using Switch 1-5: 76800 (Switch 5 on, all others off).
+                    After changing, the device must be restarted.</li>
+                    <li>Set the device to the correct address using Switch 1-5: 16 (Switch 5 on).</li>
+                    </ul>
+                    """
+
+    def __init__(self) -> None:
+        """Initialize the device class and the instrument parameters."""
+        super().__init__()
+
+        self.shortname = "AR 12S1G3"
+
+        # Communication Parameters
+        self.port_string: str = ""
+        self.port_manager = True
+        self.port_types = ["GPIB"]
+        self.port_properties = {
+            "EOL": "\r\n",
+            "timeout": 3,
+            "baudrate": 76800,
+            "stopbits": 2,
+            # "delay": 0.02,
+        }
+
+        # Device parameters
+        self.device_address: int = 16
+
+    def set_GUIparameter(self) -> dict:  # noqa: N802
+        """Returns a dictionary with keys and values to generate GUI elements in the SweepMe! GUI."""
+        return {
+            "SweepMode": ["Amplification"],
+            "Device address": 16,
+        }
+
+    def get_GUIparameter(self, parameter: dict) -> None:  # noqa: N802
+        """Receive the values of the GUI parameters that were set by the user in the SweepMe! GUI."""
+        self.port_string = parameter["Port"]
+        self.device_address = int(parameter["Device address"])
+
+    def connect(self) -> None:
+        """Connect to the device. This function is called only once at the start of the measurement."""
+
+    def initialize(self) -> None:
+        """Initialize the device. This function is called only once at the start of the measurement."""
+        self.port.write("R")  # Reset the device
+
+    def configure(self) -> None:
+        """Configure the device. This function is called every time the device is used in the sequencer."""
+        self.port.write("P1")  # Power on
+
+    def unconfigure(self) -> None:
+        """Unconfigure the device. This function is called when the procedure leaves a branch of the sequencer."""
+        self.port.write("P0")  # Power off
+
+    def apply(self) -> None:
+        """'apply' is used to set the new setvalue that is always available as 'self.value'."""
+        self.set_gain(self.value)
+
+    def set_gain(self, gain: int) -> None:
+        """Set the gain of the device."""
+        if gain < 0 or gain > 4095:
+            msg = "Gain must be between 0 and 4095."
+            raise ValueError(msg)
+
+        gain = str(gain)
+        # Add 0 to the beginning of the string if the length is less than 4
+        while len(gain) < 4:
+            gain = "0" + gain
+
+        self.port.write(f"G{gain}")
