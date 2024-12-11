b0fd7cf5ff9e815a6da66a07b2f4541080dbbeb1
Axel Fischer <axel.fischer@sweep-me.net>
2024-12-11 22:50:15 +0100

    Lumiloop LSPM -> working version, retrieves power for up to 3 channels, device selected via serial number, various parameters can be set.

--- src/Logger-LUMILOOP_LSPM/main.py
+++ src/Logger-LUMILOOP_LSPM/main.py
@@ -31,6 +31,8 @@
 # * Instrument: LUMILOOP LPSM
 
 
+import numpy as np
+
 from pysweepme.EmptyDeviceClass import EmptyDevice
 from pysweepme.ErrorMessage import debug, error
 
@@ -45,23 +47,28 @@ class Device(EmptyDevice):
         super().__init__()
 
         self.shortname = "LPSM"
-        self.variables = ["Power1", "Power2", "Power3"]
-        self.units = ["dBm", "dBm", "dBm"]
-        self.plottype = [True, True, True]
-        self.savetype = [True, True, True]
 
         self.port_manager = True
-        self.port_types = ["TCPIP", "SOCKET"]
+        self.port_types = ["TCPIP"]  # could be extended to SOCKET to circumvent use of VISA runtime
 
         self.port_properties = {
             "TCPIP_EOLwrite": "\n",
             "TCPIP_EOLread": "\n",
+            "timeout": 2,
         }
 
     def set_GUIparameter(self):
 
         gui_parameter = {
             "Serial number": "",  # could be also changed to serial number for correct identification
+            "Channel 1": True,
+            "Channel 2": False,
+            "Channel 3": False,
+            "Mode": ["0", "1", "2", "3"],
+            "Compensation frequency in Hz": "1e9",
+            "Crossover frequency in Hz": "0",
+            "Automatic video bandwidth": True,
+            "Video bandwidth in Hz": "3e6",
         }
 
         return gui_parameter
@@ -69,49 +76,117 @@ class Device(EmptyDevice):
     def get_GUIparameter(self, parameter):
 
         self.sn = int(parameter["Serial number"])
+        self.channels = {}
+        for i in range(1,4):
+            self.channels[i] = parameter[f"Channel {i}"]
+        self.all_channel_indices = [i-1 for i in self.channels if self.channels[i]]
+
+        self.variables = [f"Power {i}" for i in self.channels if self.channels[i]]
+        self.units = ["dBm" for i in self.channels if self.channels[i]]
+        self.plottype = [True for i in self.channels if self.channels[i]]
+        self.savetype = [True for i in self.channels if self.channels[i]]
+
+        self.mode = int(parameter["Mode"])
+        self.frequency = float(parameter["Compensation frequency in Hz"])
+        self.lhfrequency = float(parameter["Crossover frequency in Hz"])
+        self.automatic_video_bandwidth = parameter["Automatic video bandwidth"]
+        self.video_bandwidth = float(parameter["Video bandwidth in Hz"])
 
     def connect(self):
         pass
-        # TODO: Find power meter index for given serial number
+
+        # Problems
+        # *RST -> How to wait for the reset before continuing with further commands, *OPC? seems to respond immediately
+        # :SYST:COU? -> no response Error -113 Undefined header
+        # :MEASure[:PMeter]:RDY? [<MPMeter>] -> no response Error -113 Undefined header
+        #
 
     def initialize(self):
 
         identifier = self.get_identification()
         print("Identifier:", identifier)
 
-        count_meters = self.get_meter_count()
-        print("Number of power meters:", count_meters)
+        self.clear()
 
-        for i in range(1, count_meters+1):
-            print()
-            print(f"Power meter {i} properties")
-            print("Mode:", self.get_meter_mode(i))
-            print("Maker:", self.get_meter_maker(i))
-            print("Model:", self.get_meter_model(i))
-            print("Version:", self.get_meter_version(i))
-            print("Firmware:", self.get_meter_firmware(i))
-            print("SN:", self.get_meter_serial_number(i))
-            print("Ready:", self.get_meter_ready(i))
+        # self.reset()  # does not work yet as the reset is still ongoing when further commands are sent
+
+        stb = self.get_status_byte()
+        print("STB:", stb)
+
+        opc = self.is_operation_complete()
+        print("OPC:", opc)
+
+        # count_meters = self.get_meter_count()  # does not work
+        # print("Number of power meters:", count_meters)
+
+        self.port.write(f":MPM:SER {self.sn}, {self.sn}")
+        self.pm_index = self.sn
+
+        print()
+        print(f"Power meter {self.pm_index} properties")
+        print("Channels:", self.get_meter_channels(self.pm_index))
+        print("Version:", self.get_meter_version(self.pm_index))
+        print("Model:", self.get_meter_model(self.pm_index))
+        print("Firmware:", self.get_meter_firmware(self.pm_index))
+        print("SN:", self.get_meter_serial_number(self.pm_index))
+        print("Maker:", self.get_meter_maker(self.pm_index))
+        # print("Ready:", self.get_meter_ready(self.pm_index))
+
+        ready = self.is_system_ready(self.pm_index)
+        print("Ready:", ready)
+
+        if not ready:
+            msg = "System not ready. Please wait until temperature has reached."
+            raise Exception(msg)
+
+    def deinitialize(self):
+        self.print_all_errors()
 
     def configure(self):
 
-        # Video bandwidth
+        # Mode
+        self.set_system_mode(self.pm_index, self.mode)
+        mode = self.get_system_mode(self.pm_index)
+        print("Mode:", mode)
 
-        # Low pass frequency
+        self.max_frequency = self.get_maximum_frequency(self.pm_index)
+        self.min_frequency = self.get_minimum_frequency(self.pm_index)
+
+        if self.frequency < self.min_frequency:
+            msg = "Selected compensation frequency below calibration range."
+            raise Exception(msg)
+
+        if self.frequency > self.max_frequency:
+            msg = "Selected compensation frequency above calibration range."
+            raise Exception(msg)
+
+        # Compensation Frequency
+        self.set_frequency(self.pm_index, self.frequency)
+        frequency = self.get_frequency(self.pm_index)
+        print("Frequency in Hz:", frequency)
+
+        # Low-high frequency
+        self.set_frequency(self.pm_index, self.lhfrequency)
+        frequency = self.get_frequency(self.pm_index)
+        print("LH Frequency in Hz:", frequency)
+
+        # Video bandwidth
+        if self.automatic_video_bandwidth:
+            self.set_automatic_video_bandwidth(self.pm_index, self.automatic_video_bandwidth)
+        else:
+            self.set_automatic_video_bandwidth(self.pm_index, not self.automatic_video_bandwidth)
+            self.set_video_bandwidth(self.pm_index, self.video_bandwidth)
 
         # Triggering
 
         # Sweep times
 
-        # Frequencies
-
-        self.print_all_errors()
-
     def measure(self):
-        self.p1, self.p2, self.p3 = self.get_meter_power(1)  # retrieve power values from 1st device
+        self.results = self.get_meter_power(self.pm_index)  # retrieves all power values from 1st device
 
     def call(self):
-        return self.p1, self.p2, self.p3
+
+        return list(np.array(self.results)[self.all_channel_indices])
 
     def print_all_errors(self):
 
@@ -126,6 +201,27 @@ class Device(EmptyDevice):
         answer = self.port.read()
         return answer
 
+    def reset(self):
+        self.port.write("*RST")
+
+    def clear(self):
+        self.port.write("*CLS")
+
+    def get_status_byte(self):
+        self.port.write("*STB?")
+        answer = self.port.read()
+        return int(answer)
+
+    def is_operation_complete(self):
+        self.port.write("*OPC?")
+        answer = self.port.read()
+        return int(answer)
+
+    def is_system_ready(self, index: int):
+        self.port.write(f":SYST:RDY? {index}")
+        answer = self.port.read()
+        return int(answer)
+
     def get_error(self):
         self.port.write(":SYST:ERR?")
         answer = self.port.read()
@@ -137,14 +233,14 @@ class Device(EmptyDevice):
         return int(answer)
 
     def get_meter_count(self):
-        self.port.write(":SYST:COUN?")
+        self.port.write(":SYST:COU?")
         answer = self.port.read()
         return int(answer)
 
     def get_meter_serial_number(self, index: int):
         self.port.write(f":SYST:SER? {index}")
         answer = self.port.read()
-        return answer
+        return int(answer)
 
     def get_meter_maker(self, index: int):
         self.port.write(f":SYST:MAK? {index}")
@@ -172,11 +268,66 @@ class Device(EmptyDevice):
         return int(answer)
 
     def get_meter_ready(self, index: int):
-        self.port.write(f":MEAS:RDY? {index}")
+        self.port.write(f":MEAS:READY? {index}")
         answer = self.port.read()
         return bool(int(answer))
 
     def get_meter_power(self, index: int):
         self.port.write(f":MEAS:ALL? {index}")
         answer = self.port.read()
-        return list(map(float, answer))
+        return list(map(float, answer.split(",")))
+
+    def get_meter_channels(self, index: int):
+        self.port.write(f"SYST:CHAN? {index}")
+        answer = self.port.read()
+        return int(answer)
+
+    def set_system_mode(self, index: int, mode: int):
+        self.port.write(f":SYST:MOD {mode}, {index}")
+
+    def get_system_mode(self, index: int):
+        self.port.write(f":SYST:MOD? {index}")
+        answer = self.port.read()
+        return int(answer)
+
+    def set_frequency(self, index: int, frequency: float):
+        self.port.write(f":SYST:FREQ {frequency}, {index}")
+
+    def get_frequency(self, index: int):
+        self.port.write(f":SYST:FREQ? {index}")
+        answer = self.port.read()
+        return float(answer)
+
+    def get_minimum_frequency(self, index: int):
+        self.port.write(f":SYST:FREQ:MIN? {index}")
+        answer = self.port.read()
+        return float(answer)
+
+    def get_maximum_frequency(self, index: int):
+        self.port.write(f":SYST:FREQ:MAX? {index}")
+        answer = self.port.read()
+        return float(answer)
+
+    def set_LHfrequency(self, index: int, frequency: float):
+        self.port.write(f":SYST:LHF {frequency}, {index}")
+
+    def get_LHfrequency(self, index: int):
+        self.port.write(f":SYST:LHF? {index}")
+        answer = self.port.read()
+        return float(answer)
+
+    def set_automatic_video_bandwidth(self, index: int, state: bool):
+        self.port.write(f":MEAS:AUTOVBW {int(state)}, {index}")
+
+    def is_automatic_video_bandwidth(self, index: int):
+        self.port.write(f":MEAS:AUTOVBW? {index}")
+        answer = self.port.read()
+        return int(answer)
+
+    def set_video_bandwidth(self, index: int, frequency: float):
+        self.port.write(f":MEAS:VBW {frequency}, {index}")
+
+    def get_video_bandwidth(self, index: int):
+        self.port.write(f":MEAS:VBW? {index}")
+        answer = self.port.read()
+        return float(answer)
