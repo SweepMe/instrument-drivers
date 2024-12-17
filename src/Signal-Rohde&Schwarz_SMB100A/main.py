ca27e751a7a4a212c78fe89a1042959155fe2220
Franz Hempel <franz.hempel@sweep-me.net>
2024-12-17 12:21:52 +0100

    SMB100A -> Add configuration parameters and functions

--- src/Signal-Rohde&Schwarz_SMB100A/main.py
+++ src/Signal-Rohde&Schwarz_SMB100A/main.py
@@ -24,7 +24,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+import numpy as np
 # SweepMe! driver
 # * Module: Signal
 # * Instrument: Rohde&Schwarz SMB100A
@@ -46,70 +46,57 @@ class Device(EmptyDevice):
             "TCPIP",
         ]
 
-        self.variables = ["Frequency", "Power", "Impedance"]
-        self.units = ["Hz", "dBm", "Ohm"]
-        self.plottype = [True, True, True]
-        self.savetype = [True, True, True]
+        self.variables = ["Frequency", "Amplitude"]
+        self.units = ["Hz", "V"]
+        self.plottype = [True, True]
+        self.savetype = [True, True]
 
         # Measurement Parameters
         self.sweep_mode: str = "None"
+        self.frequency_input: str = "Frequency in Hz"  # or "Period in s"
         self.frequency: float = 2.2e9
+
+        self.amplitude_mode: str = "Amplitude in V"  # or "High level in V"
         self.amplitude: float = 0.1
 
+        self.delay_mode: str = "Phase in deg"  # or "Delay in s"
+        self.delay: float = 0.
 
     def set_GUIparameter(self) -> dict:  # noqa: N802
         """Set the standard GUI parameters."""
         return {
-            "SweepMode": ["None", "Frequency in Hz", "Power in dBm"],
-            # "Waveform": ["Pulse"],
-            "PeriodFrequency": ["Frequency in Hz"],
+            "SweepMode": ["None", "Frequency in Hz", "Amplitude in V"],
+            "Waveform": ["Sinus"],
+            "PeriodFrequency": ["Frequency in Hz", "Period in s"],
             "PeriodFrequencyValue": 2e-6,
-            "AmplitudeHiLevel": ["Amplitude in V"],
+            "AmplitudeHiLevel": ["Amplitude in V", "High level in V"],
             "AmplitudeHiLevelValue": 1.0,
             # "OffsetLoLevel": ["Low level in V"],  # "Offset in V"],
             # "OffsetLoLevelValue": 0.0,
-            # "DelayPhase": ["Delay in s"],  # "Phase in deg"],
-            # "DelayPhaseValue": 0.0,
+            "DelayPhase": ["Delay in s", "Phase in deg"],
+            "DelayPhaseValue": 0.0,
             # "DutyCyclePulseWidth": ["Pulse width in s"],  # "Duty cycle in %",],
             # "DutyCyclePulseWidthValue": 1e-6,
             # "RiseTime": 100e-9,
             # "FallTime": 100e-9,
-            # "Impedance": ["50", "1e6"],
+            "Impedance": ["50"],
             # "OperationMode": ["Range 20 V (slow)", "Range 5 V (fast)"]
         }
 
     def get_GUIparameter(self, parameter: dict) -> None:  # noqa: N802
         """Get the GUI parameters."""
-        self.port_string = parameter["Port"]
-
         self.sweep_mode = parameter["SweepMode"]
+        self.frequency_input = parameter["PeriodFrequency"]
         self.frequency = float(parameter["PeriodFrequencyValue"])
+
+        self.amplitude_mode = parameter["AmplitudeHiLevel"]
         self.amplitude = float(parameter["AmplitudeHiLevelValue"])
 
-        # self.identifier = "Keithley_4200-SCS_" + self.port_string
-        #
-        # self.waveform = parameter["Waveform"]
-        # self.periodfrequency = parameter["PeriodFrequency"]
-        # self.periodfrequencyvalue = float(parameter["PeriodFrequencyValue"])
-        # self.amplitudehilevel = parameter["AmplitudeHiLevel"]
-        # self.amplitudehilevelvalue = float(parameter["AmplitudeHiLevelValue"])
-        # self.offsetlolevel = parameter["OffsetLoLevel"]
-        # self.offsetlolevelvalue = float(parameter["OffsetLoLevelValue"])
-        # self.dutycyclepulsewidth = parameter["DutyCyclePulseWidth"]
-        # self.dutycyclepulsewidthvalue = float(parameter["DutyCyclePulseWidthValue"])
-        # self.delayphase = parameter["DelayPhase"]
-        # self.delayphasevalue = float(parameter["DelayPhaseValue"])
-        # self.impedance_value = parameter["Impedance"]
-        #
-        # self.rise_time = parameter["RiseTime"]
-        # self.fall_time = parameter["FallTime"]
-        #
-        # self.channel = parameter["Channel"]
-        #
-        # self.card_name = self.channel.split("-")[0].strip()
-        # self.pulse_channel = int(self.channel.split("-")[1][-1])
-        #
-        # self.shortname = "4200-SCS %s" % parameter["Channel"]
+        self.delay_mode = parameter["DelayPhase"]
+        self.delay = float(parameter["DelayPhaseValue"])
+
+        self.variables = [self.frequency_input.split(" ")[0], self.amplitude_mode.split(" ")[0]]
+        self.units = [self.frequency_input.split(" ")[-1], self.amplitude_mode.split(" ")[-1]]
 
     def connect(self) -> None:
         """Connect to the device."""
@@ -124,29 +111,47 @@ class Device(EmptyDevice):
 
     def configure(self) -> None:
         """Configure the device."""
-        self.set_manual_control(False)
+        self.lock_user_interface(True)
 
         self.set_power_mode("CW")
         self.set_power_unit("V")
+
         self.set_power_level(self.amplitude)
 
         self.set_frequency_mode("CW")
+
+        if self.frequency_input == "Period in s":  # else it is "Frequency in Hz"
+            self.frequency = 1 / self.frequency
         self.set_frequency(self.frequency)
+
+        # Set phase
+        phase_degree = self.delay if self.delay_mode == "Phase in deg" else self.delay * 360 / self.frequency
+        self.set_phase(phase_degree % 360)
 
         self.set_output_state(True)
 
     def unconfigure(self) -> None:
         """Unconfigure the device."""
+        self.port.write("SYST:ERR:ALL?")
+        ret = self.port.read()
+        print(ret)
+
         self.set_output_state(False)
-        self.set_manual_control(True)
+        self.lock_user_interface(False)
 
     def apply(self) -> None:
         """Apply new sweep value."""
         self.value = float(self.value)
 
         if self.sweep_mode.startswith("Frequency"):
+            if self.value < 100e3 or self.value > 12.75e6:
+                msg = "Frequency out of range. Choose between 100 kHz and 12.75 MHz."
+                raise ValueError(msg)
             self.set_frequency(self.value)
         elif self.sweep_mode.startswith("Power"):
+            if self.value < 0 or self.value > 7.071:
+                msg = "Power out of range. Choose between 0 and 7.071 V."
+                raise ValueError(msg)
             self.set_power_level(self.value)
 
     def measure(self) -> None:
@@ -156,8 +161,8 @@ class Device(EmptyDevice):
         """Return the measured values."""
         measured_frequency = self.get_frequency()
         measured_power = self.get_power_level()
-        measured_impedance = self.get_impedance()
-        return measured_frequency, measured_power, measured_impedance
+        # measured_impedance = self.get_impedance()
+        return measured_frequency, measured_power  # , measured_impedance
 
     """Wrapper Functions"""
 
@@ -181,14 +186,45 @@ class Device(EmptyDevice):
         state = "SWE" if mode == "SWE" else "CW"
         self.port.write(f":SOUR:FREQ:MODE {state}")
 
+    def set_phase(self, phase_degree: float) -> None:
+        """Sets the phase variation relative to the current phase."""
+        # Set the phase variation relative to the current phase
+        phase_radians = phase_degree * np.pi / 180
+        self.port.write(f"SOUR:PHAS {phase_radians}")
+
+        # Adopt the set phase as the current phase
+        self.port.write("PHAS:REF")
+
     def set_power_level(self, power_level: float) -> None:
         """Set the power level of the RF output."""
-        self.port.write(f"SOUR:POW:POW {power_level}")
+        if self.amplitude_mode == "Amplitude in V":
+            # This one is without offset
+            self.port.write(f"SOUR:POW:POW {power_level}")
+        elif self.amplitude_mode == "High level in V":
+            # this one is with offset
+            # TODO: See SOURce:POWer Subsystem
+            self.port.write(f"SOUR:POW:LEV:IMM:AMPL {power_level}")
 
     def get_power_level(self) -> float:
         """Get the power level of the RF output."""
-        self.port.write("SOUR:POW:POW?")
+        if self.amplitude_mode == "Amplitude in V":
+            # This one is without offset
+            self.port.write("SOUR:POW:POW?")
+        elif self.amplitude_mode == "High level in V":
+            # this one is with offset
+            self.port.write("SOUR:POW:LEV:IMM:AMPL?")
+
         return float(self.port.read())
+
+    def set_rf_mode(self, mode: str = "normal") -> None:
+        """Low distortion, low noise, or normal."""
+        # TODO
+        level_modes = {
+            "Normal": "NORM",
+            "Low noise": "LOWN",
+            "Low distortion": "LOWD",
+        }
+        self.port.write(f"SOURC:POW:LMODE {level_modes[mode]}")
 
     def set_power_mode(self, mode: str) -> None:
         """Set the power mode of the RF output to either 'CW' or 'Sweep'."""
@@ -208,12 +244,12 @@ class Device(EmptyDevice):
         return self.port.read()
 
     def set_output_state(self, set_on: bool) -> None:
-        """Set the HF output state."""
+        """Set the RF output state."""
         state = "ON" if set_on else "OFF"
         self.port.write(f":OUTP {state}")
 
     def get_output_state(self) -> bool:
-        """Get the HF output state."""
+        """Get the RF output state."""
         self.port.write("OUTP?")
         state = self.port.read()
         return state == 1
@@ -221,16 +257,24 @@ class Device(EmptyDevice):
     def get_impedance(self) -> float:
         """Get the impedance of the RF output."""
         self.port.write(":OUTP:IMP?")
-        return float(self.port.read())
+        answer = self.port.read()
+        # TODO: Check if this is correct and/or needed
+        if answer == "G50":
+            impedance = 50
+        elif answer == "G1K":
+            impedance = 1000
+        else:
+            impedance = -1
+        return impedance
 
-    def set_manual_control(self, is_locked: bool) -> None:
+    def lock_user_interface(self, is_locked: bool) -> None:
         """Lock or unlock manual control through the keyboard of the device."""
-        state = "ON" if is_locked else "OFF"
-        self.port.write(f"SYST:KLOC {state}")
+        state = "DONL" if is_locked else "ENAB"
+        self.port.write(f"SYST:ULOC {state}")
 
     def get_manual_control(self) -> bool:
         """Get the manual control state. Return True if locked, False if unlocked."""
-        self.port.write("SYST:KLOC?")
+        self.port.write("SYST:ULOC?")
         state = self.port.read()
         return state == 1
 
@@ -254,6 +298,8 @@ class Device(EmptyDevice):
     SOURce Subsystem for configuring digital and analog signals.
 
     Subsystems: AM, CORR, FM, FREQU, INP, LIST, MOD, PGEN, PHAS, PM, POW, PULM, ROSC, STER, SWE
+
+    Set RF limit:SOUR:POW:LIM:AMPL
     """
     # Probably need AM functions
 
