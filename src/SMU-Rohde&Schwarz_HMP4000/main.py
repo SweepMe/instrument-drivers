af3b7b755251dab70f4b58420518069d7e30b787
Franz Hempel <franz.hempel@sweep-me.net>
2024-12-20 13:29:21 +0100

    HMP4000 -> add docstrings, type annotations

--- src/SMU-Rohde&Schwarz_HMP4000/main.py
+++ src/SMU-Rohde&Schwarz_HMP4000/main.py
@@ -27,77 +27,85 @@
 
 # Contribution: We like to thank D M S Sultan for providing the initial version of this driver.
 
-# SweepMe! device class
-# Type: SMU
-# Device: Rohde&Schwarz HMP4000
+# SweepMe! driver
+# * Module: SMU
+# * Instrument: Rohde&Schwarz HMP4000
 
 from EmptyDeviceClass import EmptyDevice
 
 
 class Device(EmptyDevice):
-    description = """
+    """Driver for Rohde&Schwarz HMP4000 Source Unit."""
 
-                  """
-
-    def __init__(self):
+    def __init__(self) -> None:
+        """Initialize the device class and the instrument parameters."""
         EmptyDevice.__init__(self)
 
+        # Communication Parameters
         self.shortname = "HMP4000"
-
-        self.variables = ["Voltage", "Current"]
-        self.units = ["V", "A"]
-        self.plottype = [True, True]  # True to plot data
-        self.savetype = [True, True]  # True to save data
-
         self.port_manager = True
         self.port_types = ["COM", "USBTMC", "TCPIP"]
+        self.port_string: str = ""
 
         self.port_properties = {
             "timeout": 1,
             "EOL": "\n",
             "baudrate": 9600,
-            # "delay": 0.05,
-            # "Exception": False,
         }
+
+        # SweepMe! Parameters
+        self.variables = ["Voltage", "Current"]
+        self.units = ["V", "A"]
+        self.plottype = [True, True]  # True to plot data
+        self.savetype = [True, True]  # True to save data
 
         self.commands = {
             "Voltage in V": "VOLT",
             "Current in A": "CURR",
         }
 
-    def set_GUIparameter(self):
-        gui_parameter = {
+        # Measurement Parameters
+        self.source: str = "Voltage in V"
+        self.protection: float = 5.0
+        self.channel: int = 1
+
+        self.v: float = 0.0
+        self.i: float = 0.0
+
+    def set_GUIparameter(self) -> dict:  # noqa: N802
+        """Returns a dictionary with keys and values to generate GUI elements in the SweepMe! GUI."""
+        return {
             "SweepMode": ["Voltage in V", "Voltage in V (with wait)", "Current in A", "Current in A (with wait)"],
             "Channel": [1, 2, 3, 4],
             "RouteOut": ["Front"],
             "Compliance": 5.0,
         }
 
-        return gui_parameter
-
-    def get_GUIparameter(self, parameter={}):
-        # print(parameter)
+    def get_GUIparameter(self, parameter: dict) -> None:  # noqa: N802
+        """Receive the values of the GUI parameters that were set by the user in the SweepMe! GUI."""
         self.port_string = parameter["Port"]
         self.source = parameter["SweepMode"]
         self.protection = parameter["Compliance"]
         self.channel = int(parameter["Channel"])
 
-    def connect(self):
+    def connect(self) -> None:
+        """Connect to the device. This function is called only once at the start of the measurement."""
         if self.port_string.startswith("TCPIP"):
             if self.port_string.endswith("INSTR"):
-                raise Exception(
+                msg = (
                     "Instrument does not support VXI-11 standard visa runtime address ending with 'INSTR'."
-                    "Only socket based connections sending with 'SOCKET' are supported.",
+                    "Only socket based connections sending with 'SOCKET' are supported."
                 )
+                raise Exception(msg)
             elif self.port_string.endswith("SOCKET"):
                 self.port.port.write_termination = "\n"
                 self.port.port.read_termination = "\n"
             else:
-                raise Exception("Unhandled TCPIP resource name.")
+                msg = "Unhandled TCPIP resource name."
+                raise Exception(msg)
 
-    def initialize(self):
-        pass
-
+    def initialize(self) -> None:
+        """Initialize the device. This function is called only once at the start of the measurement."""
         # Use the next two lines to check whether a message remains in the buffer
         # residual_message = self.port.read()
         # print("Residual message in buffer")
@@ -108,7 +116,8 @@ class Device(EmptyDevice):
         # identifier = self.port.read()
         # print("Identifier:", identifier)
 
-    def configure(self):
+    def configure(self) -> None:
+        """Configure the device. This function is called every time the device is used in the sequencer."""
         self.port.write("INST OUT%i" % self.channel)
         self.port.write("OUTP:SEL ON")
 
@@ -120,22 +129,27 @@ class Device(EmptyDevice):
             self.port.write("VOLT %1.3f" % float(self.protection))
             self.port.write("CURR 0")
 
-    def unconfigure(self):
+    def unconfigure(self) -> None:
+        """Unconfigure the device. This function is called when the procedure leaves a branch of the sequencer."""
         self.port.write("INST OUT%i" % self.channel)
         self.port.write("OUTP:SEL OFF")
 
-    def deinitialize(self):
-        pass
+    def poweron(self) -> None:
+        """Turn on the device.
 
-    def poweron(self):
+        This function is called if the measurement procedure enters a branch of the sequencer and the module has not
+        been used in the previous branch.
+        """
         self.port.write("INST OUT%i" % self.channel)
         self.port.write("OUTP:GEN ON")
 
-    def poweroff(self):
+    def poweroff(self) -> None:
+        """Turn off the device."""
         self.port.write("INST OUT%i" % self.channel)
         self.port.write("OUTP:GEN OFF")
 
-    def apply(self):
+    def apply(self) -> None:
+        """'apply' is used to set the new setvalue that is always available as 'self.value'."""
         self.port.write("INST OUT%i" % self.channel)
 
         if self.source.startswith("Voltage"):
@@ -149,7 +163,8 @@ class Device(EmptyDevice):
         if "(with wait)" in self.source:
             time.sleep(0.5)
 
-    def measure(self):
+    def measure(self) -> None:
+        """Trigger the acquisition of data."""
         self.port.write("INST OUT%i" % self.channel)
 
         self.port.write("MEAS:VOLT?")
@@ -157,7 +172,7 @@ class Device(EmptyDevice):
 
         self.port.write("MEAS:CURR?")
         self.i = float(self.port.read())
-        # print(self.v, self.i)
 
-    def call(self):
+    def call(self) -> [float, float]:
+        """Return the measurement results. Must return as many values as defined in self.variables."""
         return [self.v, self.i]
