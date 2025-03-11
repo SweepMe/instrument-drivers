a4ced9fc47b9c40a3c38033563b4a80798e035f3
Franz Hempel <franz.hempel@sweep-me.net>
2025-03-11 12:04:03 +0100

    auto formatting, order imports

--- src/Scope-Rigol_DHOseries/main.py
+++ src/Scope-Rigol_DHOseries/main.py
@@ -30,9 +30,11 @@
 # * Instrument: Rigol DHO1000 Series
 
 
-from EmptyDeviceClass import EmptyDevice
+import time
+
 import numpy as np
-import time as time
+from pysweepme.EmptyDeviceClass import EmptyDevice
+
 
 class Device(EmptyDevice):
 
@@ -41,25 +43,25 @@ class Device(EmptyDevice):
                   """
 
     def __init__(self):
-    
+
         EmptyDevice.__init__(self)
-        
+
         self.shortname = "DHO1000"
-        
+
         self.variables = ["Time"]
         self.units = ["s"]
-        self.plottype = [True] # True to plot data
+        self.plottype = [True]  # True to plot data
         self.savetype = [True]  # True to save data
-        
+
         self.port_manager = True
         self.port_types = ["TCPIP", "USB"]
-        self.port_identifications = ['RIGOL,DHO1*'] 
-       
+        self.port_identifications = ["RIGOL,DHO1*"]
+
         self.port_properties = {
-                                "timeout": 10.0
+                                "timeout": 10.0,
                                 }
 
-        
+
         self.commands = {
                         "Channel 1": "CH1",
                         "Channel 2": "CH2",
@@ -71,18 +73,18 @@ class Device(EmptyDevice):
                         "Rising": "POS",
                         "Falling": "NEG",
                         }
-                        
+
         self.aquisitiontypes = {
-                                "As is": "As is", 
-                                "Continuous": ":RUN", 
+                                "As is": "As is",
+                                "Continuous": ":RUN",
                                 "Single": ":SING",
                                 }
 
     def set_GUIparameter(self):
 
-        GUIparameter = { 
+        gui_parameter = {
                          "SweepMode": ["None"],
-            
+
                          "TriggerSlope": ["As is", "Rising", "Falling"],
                          "TriggerSource": ["As is", "CHAN1", "CHAN2", "CHAN3", "CHAN4", "EXT", "ACL"],
                          "TriggerCoupling": ["As is", "AC", "DC", "LFReject", "HFReject"],
@@ -95,23 +97,23 @@ class Device(EmptyDevice):
                          "1e-03", "2e-03", "5e-03", "10e-03", "20e-03", "50e-03", "100e-03", "200e-03"],
                          "TimeOffsetValue": 0.0,
                          "ADCResolution": ["12bit", "14bit BW limited", "16bit BW limited"],
-                         #"SamplingRate": int(16e9), #just supported as a query by SCPI command set; Rigol sets sampling rate via SCPI in relation to chosen memory depth and timebase scale 
+                         #"SamplingRate": int(16e9), #just supported as a query by SCPI command set; Rigol sets sampling rate via SCPI in relation to chosen memory depth and timebase scale
                          "Acquisition": list(self.aquisitiontypes.keys()),
                          "Average": ["None", "2", "4", "8", "16", "32", "64", "128", "256", "512", "1024"],
                          "VoltageRange": ["Voltage range in V"],
                        }
-                       
-        for i in range(1,5):
-            GUIparameter["Channel%i" % i] = True if i == 1 else False
-            GUIparameter["Channel%i_Name" % i] = "CH%i" % i
-            GUIparameter["Channel%i_Range" % i] = ["2e-2", "4e-2", "8e-2", "2e-1", "4e-1", "8e-1", "2", "4", "8", "20", "40", "80", "200"]
-            GUIparameter["Channel%i_Offset" % i] = 0.0
-                     
-        return GUIparameter
-        
 
-    def get_GUIparameter(self, parameter={}):
-    
+        for i in range(1,5):
+            gui_parameter["Channel%i" % i] = i == 1
+            gui_parameter["Channel%i_Name" % i] = "CH%i" % i
+            gui_parameter["Channel%i_Range" % i] = ["2e-2", "4e-2", "8e-2", "2e-1", "4e-1", "8e-1", "2", "4", "8", "20", "40", "80", "200"]
+            gui_parameter["Channel%i_Offset" % i] = 0.0
+
+        return gui_parameter
+
+
+    def get_GUIparameter(self, parameter: dict):
+
         self.triggersource = parameter["TriggerSource"]
         self.triggercoupling = parameter["TriggerCoupling"]
         self.triggerslope = parameter["TriggerSlope"]
@@ -120,61 +122,61 @@ class Device(EmptyDevice):
         self.triggertimeout = int(parameter["TriggerTimeout"])
         self.adcresolution = parameter["ADCResolution"]
         self.aquisitiontype = parameter["Acquisition"]
-        
+
         self.timerange = parameter["TimeRange"]
         self.timerangevalue = parameter["TimeRangeValue"]
         self.timeoffsetvalue = parameter["TimeOffsetValue"]
         #self.samplingrate = parameter["SamplingRate"]
 
         self.average = parameter["Average"]
-        
+
         self.channels = []
         self.channel_names = {}
         self.channel_ranges = {}
         self.channel_divs = {}
         self.channel_offsets = {}
-        
+
         for i in range(1,5):
-            
+
             if parameter["Channel%i" % i]:
                 self.channels.append(i)
-                
+
                 self.variables.append(self.commands["Channel %i" % i] + " " + parameter["Channel%i_Name" % i])
                 self.units.append("V")
                 self.plottype.append(True)
                 self.savetype.append(True)
-                
+
                 self.channel_names[i] = parameter["Channel%i_Name" % i]
                 self.channel_ranges[i] = float(parameter["Channel%i_Range" % i])
                 self.channel_divs[i] = self.channel_ranges[i] / 8
                 self.channel_offsets[i] = parameter["Channel%i_Offset" % i]
 
-        
-    def initialize(self): 
+
+    def initialize(self):
         # This driver does not use Reset yet so that user can do measurements with changing options manually
         #self.port.write("*RST")
-        
-        # clears waveform on screen; 
+
+        # clears waveform on screen;
         # IMPORTANT! While it is not documented in the programming manual, this also seems to clear the waveform in the memory.
-        # If this step is skipped and the driver is run without a successful trigger, essentially running into timeout, 
+        # If this step is skipped and the driver is run without a successful trigger, essentially running into timeout,
         # it will return the same waveform again that was aquired on the last successful run before as that is still persistent in memory.
         self.port.write(":CLE")
-        
+
         # stops running aquisitions, if any are active
         self.port.write(":STOP")
 
         if len(self.channels) == 0:
             msg=("Please select at least one channel to be read out")
             raise Exception(msg)
-       
+
         # sets encoding to ASCii insetad of BYTE or WORD
         self.port.write("WAV:FORM ASC")
         # normal waveform capture, means capturing screen content
         self.port.write("WAV:MODE NORM")
-        
-                
+
+
     def configure(self):
-        
+
         ### Acquisition ###
         # setting averaging and bit depth for aquisition
         if self.adcresolution.startswith("12"):
@@ -187,28 +189,28 @@ class Device(EmptyDevice):
             if self.average !="None":
                 msg=("This scope does not allow combining averaging with HighRes ADC mode")
                 raise Exception(msg)
-                
+
             # use BW limited HighRes Mode if bit depth higher than 12 is selected and AVERAGING is not activated
             self.port.write("ACQ:TYPE HRES")
             # use only first two letters to extract bit depth and use it as SCPI command parameter
             self.port.write("ACQ:BITS %.2s" % self.adcresolution)
-        
+
         # sets the memory depth automatically and with that, sampling rate as well
-        self.port.write("ACQ:MDEP AUTO") 
-        
+        self.port.write("ACQ:MDEP AUTO")
+
         ### Trigger ###
         # setting trigger coupling
         self.port.write("TRIG:COUP %s" % self.triggercoupling)
-        
+
         # setting trigger slope and level
         if self.triggersource == "As is":
             pass
         else:
             self.port.write("TRIG:EDGE:SOUR %s" % self.triggersource)
-        
+
         # set trigger level and makes sure its format fits into SCPI standard
         self.port.write("TRIG:EDGE:LEV %s" % str("{:.4E}".format(float(self.triggerlevel))))
-        
+
         # set trigger slope
         if self.triggerslope == "As is":
             pass
@@ -216,7 +218,7 @@ class Device(EmptyDevice):
             self.port.write("TRIG:EDGE:SLOP POS")
         elif self.triggerslope == "Falling":
             self.port.write("TRIG:EDGE:SLOP NEG")
-            
+
         # makes sure that NORMAL sweep (showing only triggered events) instead of AUTO is chosen for continuous triggering
         if self.aquisitiontype == "Continuous":
             self.port.write("TRIG:SWE NORM")
@@ -235,8 +237,8 @@ class Device(EmptyDevice):
         elif self.timerange == "Time scale in s/div":
             # set timebase scale
             self.port.write("TIM:MAIN:SCAL %s" % t_div)
-        
-        # setting timebase offset    
+
+        # setting timebase offset
         self.port.write("TIM:MAIN:OFFS %s" % self.timeoffsetvalue)
 
         ### Channel properties ###
@@ -253,11 +255,11 @@ class Device(EmptyDevice):
             else:
                 # turn off unselected channels
                 self.port.write("CHAN%s:DISP OFF" % i)
-                
-            
+
+
     def apply(self):
         pass
-        
+
     def measure(self):
 
         time.sleep(float(self.triggerdelay))
@@ -272,13 +274,13 @@ class Device(EmptyDevice):
                 raise Exception(msg)
             # will execute either :RUN oder :SINGle SCPI command depending on choice of aquisition type (continuous or single)
             self.port.write("%s" % self.aquisitiontypes[self.aquisitiontype])
-        
+
         time.sleep(1)
         # The RIGOL does not allow to determine the amount of successful acquired averaging samples.
         # It also returns TD for trigger status during continuous triggering when a trigger was successful, but already waiting for a new one.
         # Therefore, the only solution is to STOP the scope after a manual set trigger timeout period which has to be adjusted accordingly.
         # For normal (non-averaging) use, this is not a problem.
-        
+
         while True:
             # check if trigger counter ran into timeout limit;
             if trigcounter >= self.triggertimeout:
@@ -300,11 +302,11 @@ class Device(EmptyDevice):
             else:
                 trigcounter += 1
                 time.sleep(1)
-                
+
 
         self.port.write("WAV:PRE?")
         preamble = self.port.read().split(",")
-            
+
         # splitting preamble values into seperate variables for further use
         wav_format=preamble[0]
         acq_mode=preamble[1]
@@ -316,36 +318,36 @@ class Device(EmptyDevice):
         y_inc=float(preamble[7])
         y_orig=float(preamble[8])
         y_ref=float(preamble[9])
-        
+
         slot = 0
-        
+
         for i in self.channels:
-            if 'inputarray' in locals():
+            if "inputarray" in locals():
                 del inputarray
-            
+
             #create empty inputarray array
             inputarray = []
-            
+
             # select channel to be read
             self.port.write("WAV:SOUR CHAN%s" % i)
-            
+
             self.port.write("WAV:DATA?")
             inputarray=self.port.read().split(",")
-            
+
             # only for first measurement
-            if slot == 0: 
+            if slot == 0:
                 # number of measured channels
                 channels = len(self.channels)
                 # generate empty array of correct size for channels + data
                 self.voltages = np.zeros((len(inputarray), len(self.channels)))
                 # generate linear time array FROM, TO, STEPSAMOUNT
                 self.timecode = np.linspace(x_orig, (x_orig+x_inc*(len(inputarray))), len(inputarray))
-                       
+
             data = []
             for n in np.arange(len(inputarray)):
                 # sort the values as data
                 data.append(inputarray[n])
-            
+
             # convert list to data array
             data = np.array(data)
 
@@ -353,6 +355,6 @@ class Device(EmptyDevice):
             self.voltages[:, slot] = data
             # set correct column for next channel
             slot += 1
-            
+
     def call(self):
-        return  [self.timecode] + [self.voltages[:,i] for i in range(self.voltages.shape[1])]
+        return [self.timecode] + [self.voltages[:,i] for i in range(self.voltages.shape[1])]
