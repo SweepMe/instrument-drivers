46d0717a29b3352341e062e7c121986e5dd94196
Franz Hempel <franz.hempel@sweep-me.net>
2025-04-28 13:16:52 +0200

    2700 -> auto formatting

--- src/Logger-Keithley_2700/main.py
+++ src/Logger-Keithley_2700/main.py
@@ -1,4 +1,4 @@
-﻿# This Device Class is published under the terms of the MIT License.
+﻿﻿# This Device Class is published under the terms of the MIT License.
 # Required Third Party Libraries, which are included in the Device Class
 # package for convenience purposes, may have a different license. You can
 # find those in the corresponding folders or contact the maintainer.
@@ -34,27 +34,27 @@
 # * Instrument: Keithley 2700
 
 
-import numpy as np
 import time
-import pyvisa
 
-from EmptyDeviceClass import EmptyDevice
+import numpy as np
+import pyvisa
+from pysweepme.EmptyDeviceClass import EmptyDevice
+
 
 class Device(EmptyDevice):
 
     description = """
                   Usage:
                   Scanning: The driver will scan through all channels of the given channel list. Otherwise the driver just returns a reading
-                  
-                  
+
                   Info:
-                  
+
                   """
 
     def __init__(self):
 
         EmptyDevice.__init__(self)
-        
+
         self.shortname = "Keithley 2700"
         self.port_manager = True
         self.port_types = ["COM", "GPIB", "TCPIP"]  # Keithley 2701 has Ethernet connection
@@ -105,7 +105,7 @@ class Device(EmptyDevice):
             "Temperature":  "K",
             "Continuity":   "",
         }
-                        
+
         # a list of available resolutions as they can be sent to the instrument
         self.resolutions = [
             "0.1",       # i.e., 100.0 V (3½ digits)
@@ -125,16 +125,16 @@ class Device(EmptyDevice):
             "Internal":  "BUS",
             "External":  "EXT",
         }
-                                
-        self.variables=['Variable1']
-        self.units=['Unit1']
+
+        self.variables=["Variable1"]
+        self.units=["Unit1"]
         self.plottype=[True]
         self.savetype=[True]
         self.had_trigger = [False]
 
 
     def set_GUIparameter(self):
-        GUIparameter = {
+        return {
                         "Mode" : list(self.modes.keys()),
                         "Digits": ["4", "5", "6", "7"],  #self.resolutions,
                         "Trigger": list(self.trigger_types.keys()),
@@ -142,32 +142,31 @@ class Device(EmptyDevice):
                         "Range": ["Auto", "0.1", "1", "10", "100", "1000", "10000", "100000", "1000000"],
                         "Temperature unit": ["°C", "K", "°F"],
                         "Scanning": False,
-                        "Channel list": "101:120", 
+                        "Channel list": "101:120",
                         }
-        return GUIparameter
 
     def get_GUIparameter(self, parameter = {}):
         #print(parameter)
-        self.mode = parameter['Mode']
-        self.digits = parameter['Digits']  # Digits of resolution
-        self.channel_string = parameter['Channel list']
-        intermediate_list = self.channel_string.replace(',',';').split(';')
+        self.mode = parameter["Mode"]
+        self.digits = parameter["Digits"]  # Digits of resolution
+        self.channel_string = parameter["Channel list"]
+        intermediate_list = self.channel_string.replace(",",";").split(";")
         self.channel_list = []
         for x in intermediate_list:
-            if ':' in x:
-                first,last=x.split(':')
+            if ":" in x:
+                first,last=x.split(":")
                 channels_to_add = list(range(int(first),int(last)+1))
                 self.channel_list+=[str(x) for x in channels_to_add]
             else:
                 self.channel_list+=[x]
-        self.channel_names = ['Dev'+x[1:] for x in self.channel_list]
+        self.channel_names = ["Dev"+x[1:] for x in self.channel_list]
         #self.channel_names = parameter['Channel names'].split(';')
         #self.channel_list = parameter['Channel list']#.replace(" ", "").replace("-", "").split(",")
-        self.trigger_type = parameter['Trigger']
-        self.scanning = parameter['Scanning']
+        self.trigger_type = parameter["Trigger"]
+        self.scanning = parameter["Scanning"]
 
-        self.range = parameter['Range']
-        self.integration_speed = parameter['Integration speed']
+        self.range = parameter["Range"]
+        self.integration_speed = parameter["Integration speed"]
 
         self.temperature_unit = str(parameter["Temperature unit"])
 
@@ -194,7 +193,7 @@ class Device(EmptyDevice):
             self.units = [self.mode_units[self.mode]]
             self.plottype = [True]  # True to plot data
             self.savetype = [True]
-            
+
     def connect(self):
         if self.port_string.startswith("TCPIP"):
             self.port.port.set_visa_attribute(pyvisa.constants.VI_ATTR_IO_PROT,4)
@@ -227,19 +226,19 @@ class Device(EmptyDevice):
             else:
                 #self.port.write("%s:range:auto off, (@%s)" % (self.modes[self.mode], self.channel_string))
                 self.port.write("%s:range %s, (@%s)" % (self.modes[self.mode], str(self.range), self.channel_string))
-            
+
             # Write number of digits resolution
             self.port.write("%s:dig %s, (@%s)" % (self.modes[self.mode], self.digits, self.channel_string) )
-        
+
         # The following configuration is incompatible with scanning, but may be needed for individual measurements:
         #self.port.write("CONF:%s %s, (@%s)" % (self.modes[self.mode], self.resolution, self.channel_string))  # we send the command of the selected mode and append range, resolution and channel list
-        
+
         # Trigger
         self.port.write("INIT:CONT OFF")  # disable continuous initiation, needed to use "READ?" command
         self.port.write("TRIG:SOUR %s" % self.trigger_types[self.trigger_type])
         self.port.write("trigger:count 1")  # Only scan through a list once
         self.port.write("TRIG:DEL 0.5")
-        
+
         # Speed
         if self.integration_speed == "Fast":
             self.nplc = 0.1
@@ -250,7 +249,7 @@ class Device(EmptyDevice):
         else:
             self.nplc = 1.0
         self.port.write(":SENS:%s:NPLC %s" % (self.modes[self.mode], str(self.nplc)))
-        
+
         # Sample count:
         # Note, sample count is the number of measurements that will be returned,
         # not the number of measurements per channel.
@@ -258,7 +257,7 @@ class Device(EmptyDevice):
             self.port.write("sample:count " + str(len(self.channel_list)))
         else:
             self.port.write("sample:count 1")
-        
+
         # Scanning
         if self.scanning:
             self.port.write("route:scan (@" + self.channel_string + ")")  # start scan in the background
@@ -267,7 +266,7 @@ class Device(EmptyDevice):
 
     def deinitialize(self):
         self.port.write("SYST:BEEP:STAT ON")  # control-Beep on
-        
+
     def trigger_ready(self):
         self.had_trigger = [False]
         self.port.write("INIT") #initialize trigger
@@ -278,24 +277,24 @@ class Device(EmptyDevice):
         #    self.port.write("form:elem READ,CHAN\n;READ?")
         #else:
         #    self.port.write("form:elem READ\n;READ?") # This returns just the reading
-        
+
     def request_result(self):
         #wait for trigger
         self.port.write("TRAC:FREE?")
-        bytes_in_buffer = self.port.read().split(',')
+        bytes_in_buffer = self.port.read().split(",")
         bytes_in_use = bytes_in_buffer[1]
-        
+
         while int(bytes_in_use) < len(self.channel_list)*16: #normaly 16 bytes reserved for each entry in buffer: 8 bytes per measure value, 8 bytes per timestamp
             stop_measurement = self.is_run_stopped()
             if stop_measurement:
                 break
-                
+
             self.port.write("TRAC:FREE?")
-            bytes_in_buffer = self.port.read().split(',')
+            bytes_in_buffer = self.port.read().split(",")
             bytes_in_use = bytes_in_buffer[1]
-            
+
             time.sleep(0.5)
-        
+
         if (int(bytes_in_use) > len(self.channel_list)*16):
             self.had_trigger = [True]
 
@@ -303,7 +302,7 @@ class Device(EmptyDevice):
         if self.had_trigger:
             self.port.write("form:elem READ\n;FETCh?")
             answer = self.port.read()  # here we read the response from the "READ?" request in 'measure'
-            readings_list = np.array([float(x) for x in answer.strip('\n').split(',')])
+            readings_list = np.array([float(x) for x in answer.strip("\n").split(",")])
             #print(len(readings_list))
             #print(readings_list)
             #print(len(self.channel_list))
@@ -318,6 +317,6 @@ class Device(EmptyDevice):
     # here, command-wrapping functions are defined
 
     def get_identification(self):
-    
+
         self.port.write("*IDN?")
         return self.port.read()
