be1f281e397dce6ac892a32b8f775780cd37ee1c
Franz Hempel <franz.hempel@sweep-me.net>
2025-08-21 12:00:53 +0200

    Stepper -> update stepper motor with results from working MWE

--- src/Switch-Thorlabs_StepperMotor/main.py
+++ src/Switch-Thorlabs_StepperMotor/main.py
@@ -31,13 +31,15 @@
 
 from __future__ import annotations
 
+import contextlib
+
 import sys
 import time
 from typing import Any
 
 import clr
 from pysweepme.EmptyDeviceClass import EmptyDevice
-
+from System import Decimal, Int32
 
 # Import Kinesis dll
 kinesis_imported = False
@@ -92,7 +94,8 @@ class Device(EmptyDevice):
 
         return device_list
 
-    def list_devices(self) -> list[str]:
+    @staticmethod
+    def list_devices() -> list[str]:
         """Lists all devices.
 
         Bug: Once Simulation mode is switched on, GetDeviceList will also find simulated devices even when simulation
@@ -114,14 +117,14 @@ class Device(EmptyDevice):
         del parameters
         return {
             "SweepMode": ["Position"],
-            "Bay": [1, 2, 3, 4, 5, 6, 7, 8],  # maybe just use line edit
+            "Bay": "1",  # maybe just use line edit
         }
 
     def apply_gui_parameters(self, parameters: dict[str, Any]) -> None:
         """Receive the values of the GUI parameters that were set by the user in the SweepMe! GUI."""
         self.serial_number = parameters.get("Port", "")
         self.sweep_mode = parameters.get("SweepMode", "Position")
-        self.channel = parameters.get("Bay", 1)
+        self.channel = parameters.get("Bay", "1")
 
     def connect(self) -> None:
         """Connect to the device. This function is called only once at the start of the measurement."""
@@ -140,13 +143,24 @@ class Device(EmptyDevice):
             raise ValueError(msg)
 
         device_info = DeviceManagerCLI.DeviceFactory.GetDeviceInfo(self.serial_number)
-
         self.rack = ModularRackCLI.Rack.ModularRack.CreateModularRack(device_info.GetTypeID(), self.serial_number)
 
-        self.stepper = self.rack[self.channel]
+        self.stepper = self.rack[int(self.channel)]
 
-        # Open Connection
-        self.rack.Connect(int(self.serial_number))
+        # Connect to the device
+        timeout_s = 10
+        while not self.rack.IsConnected:
+            try:
+                self.rack.Connect(self.serial_number)
+            except DeviceManagerCLI.DeviceNotReadyException:
+                #print("DeviceNotReadyException: Device is not ready yet, retrying...")
+                time.sleep(0.2)
+                timeout_s -= 0.2
+
+            if timeout_s <= 0:
+                #print("Timeout: Device connection failed after 10 seconds.")
+                msg = "Failed to connect to the device within the timeout period."
+                raise TimeoutError(msg)
 
     def disconnect(self) -> None:
         """Disconnect from the device. This function is called only once at the end of the measurement."""
@@ -158,35 +172,26 @@ class Device(EmptyDevice):
 
     def initialize(self) -> None:
         """Initialize the device. This function is called only once at the start of the measurement."""
+        # pass if the device is already initialized
         if not self.stepper.IsSettingsInitialized():
-            print("Waiting for settings to be initialized...")
-            self.stepper.WaitForSettingsInitialized(10000)  # ms
+            with contextlib.suppress(Exception):
+                self.stepper.WaitForSettingsInitialized(5000)
 
         # The polling loop requests regular status requests to the motor to ensure the program keeps track of the device.
         self.stepper.StartPolling(250)
-
-        # Needs a delay so that the current enabled state can be obtained
         time.sleep(0.5)
 
         # Enable the channel otherwise any move is ignored
         self.stepper.EnableDevice()
-
-        # Needs a delay to give time for the device to be enabled
         time.sleep(0.5)
 
-        # TODO: Check if needed to continue with stepper_motor object
-        # stepper_motor = self.rack.GetStepperChannel(self.channel)
-        # if not stepper_motor:
-        #     msg = f"Stepper channel {self.channel} not found in the rack."
-        #     raise ValueError(msg)
+        # continue with stepper_motor object - unclear why
+        self.stepper_motor = self.rack.GetStepperChannel(int(self.channel))
+        # Why?
+        motorConfiguration = self.stepper_motor.LoadMotorConfiguration(self.stepper.DeviceID)
+        currentDeviceSettings = self.stepper_motor.MotorDeviceSettings
 
         self.stepper.Home(self.timeout_ms)
-
-    def deinitialize(self) -> None:
-        """Deinitialize the device. This function is called only once at the end of the measurement."""
-        # TODO: Here or in disconnect?
-        self.stepper.StopPolling()
-        self.rack.Disconnect()
 
     def configure(self) -> None:
         """Configure the device. This function is called every time the device is used in the sequencer."""
@@ -198,15 +203,14 @@ class Device(EmptyDevice):
         """'apply' is used to set the new setvalue that is always available as 'self.value'."""
         if self.sweep_mode == "Position":
             try:
-                position = float(self.value)
+                position = Decimal(self.value)
             except ValueError as e:
-                msg = f"Invalid position format. Expected a float, got '{self.value}'."
+                msg = f"Invalid position format. Expected a integer, got '{self.value}'."
                 raise ValueError(msg) from e
 
-            # Move the stepper motor to the specified position
-            self.stepper.MoveTo(position, self.timeout_ms)
+            self.stepper_motor.MoveTo(position, Int32(self.timeout_ms))
 
-    def call(self) -> str:
+    def call(self) -> float:
         """Return the measurement results. Must return as many values as defined in self.variables."""
-        return self.stepper.Position
+        return float(str(self.stepper_motor.Position))
 
