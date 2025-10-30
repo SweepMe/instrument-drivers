db68e6946aeb92e8aaf76323a73ddba832184e56
Franz Hempel <franz.hempel@sweep-me.net>
2025-10-30 15:53:26 +0100

    B1500 -> working multi channel list mode

--- src/SMU-Agilent_B1500/main.py
+++ src/SMU-Agilent_B1500/main.py
@@ -100,7 +100,10 @@ class Device(EmptyDevice):
         # Pulse parameters - not yet implemented in the code
 
         # List sweep parameters
-        self.use_list_mode: bool = False
+        self.use_list_mode: bool = False  # whether the user has selected 'list sweep' as sweep value
+        self.list_master: bool = False  # The first channel that configures the list mode and starts the measurement
+        self.list_follower: bool = False  # If another channel is the list master, this channel becomes a list follower. No matter if it is set to single or list mode
+
         self.list_modes = {
             "linear": 1,
             "logarithmic": 2,
@@ -207,11 +210,12 @@ class Device(EmptyDevice):
         print("clearing error queue...")
         self.check_errors()
         print(self.port.query("*IDN?"))
-        driver_port_string = "Agilent_B1500_" + self.port_string
+        self.driver_port_string = "Agilent_B1500_" + self.port_string
 
+        # TODO: move to configure to allow use of list mode and non-list mode in seperate branches
         # initialize commands only need to be sent once, so we check here whether another instance of the same driver
         # AND same port did it already. If not, this instance is the first and has to do it.
-        if not driver_port_string in self.device_communication:
+        if not self.driver_port_string in self.device_communication:
             self.port.write("*RST")  # reset to initial settings
             self.port.write("BC")  # buffer clear
             self.port.write("AZ 0")  # Auto-Zero off for faster measurements
@@ -219,16 +223,55 @@ class Device(EmptyDevice):
 
             # if initialize commands have been sent, we can add the driver_port_string to the dictionary that is seen by
             # all drivers
-            self.device_communication[driver_port_string] = True
+            # TODO: could add delay and hold to compare settings with other channels
+            self.device_communication[self.driver_port_string] = {
+                "is_initialized": True,
+                "channels": [self.channel],
+                "list_master_channel": -1,
+                "list_length": 0,
+                "list_hold": 0.0,
+                "list_delay": 0.0,
+                "list_results": {},
+            }
 
-        if self.use_list_mode:
-            print("Setting format for list mode...")
-            self.port.write("FMT 2,1")  # overwrite for list mode to return the set values as well. Might need to go for 2,2 when using multiple channels
+        # if self.use_list_mode:
+        #     print("Setting format for list mode...")
+        #     self.port.write("FMT 2,1")  # overwrite for list mode to return the set values as well. Might need to go for 2,2 when using multiple channels
 
 
 
     def configure(self) -> None:
         """Configure the device. This function is called every time the device is used in the sequencer."""
+        # When entering a branch, add the channel to the device configuration
+        device_info = self.device_communication[self.driver_port_string]
+        if self.channel not in device_info["channels"]:
+            device_info["channels"].append(self.channel)
+
+        if self.use_list_mode:
+            if device_info["list_master_channel"] == -1:
+                # No list master defined yet, this channel becomes the master
+                device_info["list_master_channel"] = self.channel
+                device_info["list_length"] = self.list_steps
+                device_info["list_hold"] = self.list_hold
+                device_info["list_delay"] = self.list_delay
+                self.list_master = True
+            else:
+                # another channel is already the list master, this channel becomes a synchronous list slave
+                self.list_follower = True
+
+                if self.list_steps != device_info["list_length"]:
+                    msg = (f"List sweep length mismatch: list master channel {device_info['list_master_channel']} has "
+                           f"{device_info['list_length']} steps, but channel {self.channel} has {self.list_steps} steps.")
+                    raise ValueError(msg)
+
+                if self.list_hold != device_info["list_hold"] or self.list_delay != device_info["list_delay"]:
+                    msg = (f"List sweep timing mismatch: list master channel {device_info['list_master_channel']} has "
+                           f"hold time {device_info['list_hold']} s and delay time {device_info['list_delay']} s, but channel {self.channel} has "
+                           f"hold time {self.list_hold} s and delay time {self.list_delay} s.")
+                    raise ValueError(msg)
+
+        # the decision between list follower and single step mode should be made after all channels are signed in
+
         # The command CN has to be sent at the beginning as it resets certain parameters
         # If the command CN would be used later, e.g. durin  "poweron", it would overwrite parameters that are defined during "configure"
         self.port.write(f"CN {self.channel}")  # switches the channel on
@@ -241,8 +284,11 @@ class Device(EmptyDevice):
         self.set_average(self.average)
         self.check_errors()
 
-        if self.use_list_mode:
-            print("Configuring list mode...")
+        if self.list_master:
+            # Only the list master channels uses configure_list_mode, all others use create_synchronous_list afterward
+            print("Configuring master list mode...")
+            self.port.write("FMT 2,1")  # 2: ASCII 12 digits w/o header, 2: source output data for synchronous list mode
+            # TODO: we could say no channel returns its source values and we always calculate them
             self.configure_list_mode(
                 source=self.source[0],
                 mode=self.list_mode,
@@ -252,7 +298,7 @@ class Device(EmptyDevice):
                 steps=self.list_steps,
                 compliance=self.protection,
             )
-            self.set_measurement_mode(2, [self.channel])  # staircase sweep mode. Might need to move to poweron
+
             self.set_list_timing(self.list_hold, self.list_delay)
             # Enable automatic abort function (2). Return to start value (1) after abort
             self.port.write("WM 2, 1")
@@ -265,11 +311,38 @@ class Device(EmptyDevice):
 
     def unconfigure(self) -> None:
         """Unconfigure the device. This function is called when the procedure leaves a branch of the sequencer."""
+        # remove the channel from the device configuration
+        device_info = self.device_communication[self.driver_port_string]
+        if self.channel in device_info["channels"]:
+            device_info["channels"].remove(self.channel)
+        if self.use_list_mode and self.list_master:
+            device_info["list_master_channel"] = -1
+            device_info["list_length"] = 0
+
         # This throws an error because IN cannot be used when the channel is off from poweroff
         # self.port.write(f"IN {self.channel}")
         # self.check_errors()
         # resets to zero volt
         # self.port.write("DZ")
+
+    def signin(self) -> None:
+        """This function is called after configure."""
+        # list master can only set up measurement mode after all other channels have registered
+        if self.list_master:
+            channel_list = self.device_communication[self.driver_port_string]["channels"]
+            measurement_mode = 2 #if len(channel_list) == 1 else 16  # staircase sweep or multi channel sweep
+            self.set_measurement_mode(measurement_mode, channel_list)  # staircase sweep mode
+            self.check_errors()
+
+        # adding secondary list modes must be done after the list master configures the list mode
+
+        # here we check if another channel in the same branch is forcing the list mode, even if this channel was set to
+        # single measurement
+        elif self.device_communication[self.driver_port_string]["list_master_channel"] != -1:
+            print(f"Another channel is list master, this channel {self.channel} becomes list follower.")
+            self.list_follower = True
+        else:
+            self.list_follower = False
 
     def poweron(self) -> None:
         """In a previous version, the CN command was sent here.
@@ -283,9 +356,29 @@ class Device(EmptyDevice):
         """Turn off the device when leaving a sequencer branch."""
         self.port.write(f"CL {self.channel}")  # switches the channel off
 
+    def start(self) -> None:
+        """In start we set up list followers that have no sweep value."""
+        if self.list_follower and self.use_list_mode:
+            self.create_synchronous_list(self.list_start, self.list_stop)
+            self.check_errors()
+
     def apply(self) -> None:
         """This function is called if the set value has changed. Applies the new value available as self.value."""
-        if self.use_list_mode:
+        if self.list_master:
+            # list master channel sets up list mode in configure
+            return
+
+        if self.list_follower:
+            if self.use_list_mode:
+                # this is never reached bc we do not have a sweep value
+                self.create_synchronous_list(self.list_start, self.list_stop)
+            else:
+                # If the channel was set up for single measurement, but another channel is using list mode, we have to
+                # create a synchronous list with start = stop = value
+                value = float(self.value)
+                self.create_synchronous_list(value, value)
+
+            self.check_errors()
             return
 
         if self.source.startswith("Voltage"):
@@ -296,12 +389,34 @@ class Device(EmptyDevice):
 
         self.check_errors()
 
+    def create_synchronous_list(self, start: float, stop: float) -> None:
+        """Create a staircase sweep source that will be synchronized with the primary sweep source.
+
+        The primary source (list master) must have configured its list mode via WI or WV before calling this function.
+        Otherwise, it will be overwritten.
+        """
+        # the source number is a unique identifier. It must be between 2-10. We use the channel number + 1 here.
+        source_number = self.channel + 1
+        mode = 1 if self.source.startswith("Voltage") else 2
+        source_range = 0  # autorange, can be extended later
+        print(f"Creating synchronous list for channel {self.channel}: {start} to {stop}")
+        command = f"WNX {source_number},{self.channel},{mode},{source_range},{start},{stop}, {self.protection}"
+        # print(command)
+        # self.port.write(command)
+
+        # maybe this is the correct command instead of WNX
+        command = f"WS{self.source[0]} {self.channel}, {source_range},{start},{stop}"
+        print(command)
+        self.port.write(command)
+
+
+
     def check_errors(self) -> None:
         """Check for errors in the device error queue.
 
         122 - Number of channels must be corrected.
-Check the MM, FL, CN, CL, IN, DZ, or RZ command, and correct the
-number of channels.
+            Check the MM, FL, CN, CL, IN, DZ, or RZ command, and correct the
+            number of channels.
         """
         status = self.port.query("ERR?")
 
@@ -309,27 +424,31 @@ number of channels.
             " 100": "Undefined GPIB command.",
         }
 
-        # if status != "0,0,0,0":
-        print(status)
+        if status != "0,0,0,0":
+            print(status)
 
     def measure(self) -> None:
         """Trigger the acquisition of new data."""
-        if self.use_list_mode:
-            # TODO: only the list_master channel should execute XE?
+        if self.list_master:
             print("Starting list mode measurement...")
             self.check_errors()
             self.port.write("XE")  # execute measurement
+
+        elif self.list_follower:
+            return
+
         else:
             self.port.write(f"TI {self.channel},0")
             self.port.write(f"TV {self.channel},0")
 
     def request_result(self) -> None:
-        """Wait for the list mode to finish and request the measurement results."""
-        if self.use_list_mode:
+        """The master channel waits for the list mode to finish and request the measurement results."""
+        if self.list_master:
             timeout_s = 10
             start_time = time.perf_counter()
 
             # TODO: allow aborting the wait from outside
+            # set port timeout to a low value, check with try except until OPC returns 1
             while True:
                 if self.is_run_stopped():
                     break
@@ -340,19 +459,29 @@ number of channels.
 
     def read_result(self) -> None:
         """Read the measured data from a buffer that was requested during 'request_result'."""
-        if self.use_list_mode:
+        if self.list_master:
             print("Reading list mode results.")
             results = self.port.read()
-            values = [float(part.strip()) for part in results.split(',') if part.strip()]
-            print(values)
+            print(results)
+            values = [float(part.strip()[3:]) for part in results.split(',') if part.strip()]
+            # print(values)
+            print(len(values))
 
-            
-            if self.source.startswith("Voltage"):
-                self.measured_voltage = values[0::2]
-                self.measured_current = values[1::2]
-            else:
-                self.measured_current = values[0::2]
-                self.measured_voltage = values[1::2]
+            device_communication = self.device_communication[self.driver_port_string]
+            channels = device_communication["channels"]
+            n_channels = len(channels)
+
+            for n, channel in enumerate(channels):
+                measured_values_for_channel = values[n::n_channels + 1]  # skip source values
+                device_communication["list_results"][channel] = measured_values_for_channel
+
+            self.source_values = values[n_channels::n_channels + 1]
+
+            print(device_communication)
+
+        elif self.list_follower:
+            # list followers read results from device_communication in call
+            return
 
         else:
             answer = self.port.read()
@@ -369,6 +498,30 @@ number of channels.
 
     def call(self) -> list[float]:
         """Return the measurement results. Must return as many values as defined in self.variables."""
+        if self.list_follower or self.list_master:
+            device_communication = self.device_communication[self.driver_port_string]
+            measured_values = device_communication["list_results"][self.channel]
+
+            # Create source values list
+            list_length = device_communication["list_length"]
+            if self.use_list_mode:
+                if self.list_master:
+                    source_values = self.source_values
+                    # TODO: handle log sweep
+                else:
+                    source_values = np.linspace(self.list_start, self.list_stop, list_length).tolist()
+
+            else:
+                source_values = [float(self.value)] * list_length
+
+            # assign to measured voltage and current based on source type
+            if self.source.startswith("Voltage"):
+                self.measured_voltage = source_values
+                self.measured_current = measured_values
+            else:
+                self.measured_current = source_values
+                self.measured_voltage = measured_values
+
         return [self.measured_voltage, self.measured_current]
 
     # Wrapped functions
@@ -417,7 +570,7 @@ number of channels.
         """
         # command =
         self.port.write(f"MM {mode},{','.join(str(ch) for ch in channels)}")
-        print("Checking errors after setting measurement mode")
+        print(f"Setting up measurement mode {mode} for channels {channels}")
         self.check_errors()
 
     def set_speed(self, speed: str) -> None:
